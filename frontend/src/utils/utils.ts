d1b0b760c69bd5f2f24de7d46af3a501412c69d9
Laurynas Keturakis <laurynas.keturakis@gmail.com>
2024-05-24 01:29:48 +0200

    utils for dates

--- frontend/src/utils/utils.ts
+++ frontend/src/utils/utils.ts
@@ -1,5 +1,13 @@
-import { format } from "date-fns";
+import { format, formatDistanceToNow, formatRFC7231 } from "date-fns";
 
 export function formatDate(d: Date | string) {
-  return format(new Date(d), 'HH:mm:ss.SSS');
+  return format(new Date(d), "HH:mm:ss.SSS");
+}
+
+export function humanReadableDate(dateString: string) {
+  return formatDistanceToNow(new Date(dateString), { addSuffix: true });
+}
+
+export function rfc7231Date(dateString: string) {
+  return formatRFC7231(new Date(dateString));
 }
