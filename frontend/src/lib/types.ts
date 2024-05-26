a57a0f3d08e75edb2dc9c152b9e0974791736329
Laurynas Keturakis <laurynas.keturakis@gmail.com>
2024-05-26 17:21:37 +0200

    types

--- frontend/src/lib/types.ts
+++ frontend/src/lib/types.ts
@@ -1,6 +1,21 @@
 import type { Endpoints } from "@octokit/types";
 
-export type GitHubIssues =
-  Endpoints["GET /repos/{owner}/{repo}/issues"]["response"]["data"];
+export type GitHubResponse =
+  Endpoints["GET /repos/{owner}/{repo}/issues"]["response"];
 
-export type GitHubIssue = GitHubIssues[number] extends undefined ? never : GitHubIssues[number]
+type GitHubIssues = GitHubResponse["data"];
+
+export type GitHubIssue = GitHubIssues[number] extends undefined
+  ? never
+  : GitHubIssues[number];
+
+// TODO: share this with the backend
+export type Dependency = {
+  name: string;
+  version: string;
+  repository: {
+    owner: string;
+    repo: string;
+    url: string;
+  };
+};
