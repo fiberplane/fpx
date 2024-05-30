1b971f9f8481e079a0020a23d172149e3d2f3cb3
Jacco Flenter <flenter@users.noreply.github.com>
2024-05-30 22:11:10 +0200

    Format using spaces

--- client-library/src/legacy.ts
+++ client-library/src/legacy.ts
@@ -1,13 +1,13 @@
 import type { NeonDbError } from "@neondatabase/serverless";
 import {
-	PRETTIFY_MIZU_LOGGER_LOG,
-	errorToJson,
-	extractCallerLocation,
-	generateUUID,
-	neonDbErrorToJson,
-	polyfillWaitUntil,
-	shouldPrettifyMizuLog,
-	tryPrettyPrintLoggerLog,
+  PRETTIFY_MIZU_LOGGER_LOG,
+  errorToJson,
+  extractCallerLocation,
+  generateUUID,
+  neonDbErrorToJson,
+  polyfillWaitUntil,
+  shouldPrettifyMizuLog,
+  tryPrettyPrintLoggerLog,
 } from "./utils";
 import { RECORDED_CONSOLE_METHODS } from "./request-logger";
 
@@ -18,7 +18,7 @@ export { logger } from "./request-logger";
 // =========================== //
 
 type MizuEnv = {
-	MIZU_ENDPOINT: string;
+  MIZU_ENDPOINT: string;
 };
 
 // ====================================== //
@@ -26,84 +26,84 @@ type MizuEnv = {
 // ====================================== //
 
 export const Mizu = {
-	init: (
-		/** Configuration of Mizu backend */
-		{ MIZU_ENDPOINT: mizuEndpoint }: MizuEnv,
-		ctx: ExecutionContext,
-		/** Name of service (not in use, but will be helpful later) */
-		service?: string,
-		/** Use `libraryDebugMode` to log into the terminal what we are sending to the Mizu server on each request/response */
-		libraryDebugMode?: boolean,
-	) => {
-		// NOTE - Polyfill is probably not necessary for Cloudflare workers, but could be good for vercel envs
-		//         https://github.com/highlight/highlight/pull/6480
-		polyfillWaitUntil(ctx);
+  init: (
+    /** Configuration of Mizu backend */
+    { MIZU_ENDPOINT: mizuEndpoint }: MizuEnv,
+    ctx: ExecutionContext,
+    /** Name of service (not in use, but will be helpful later) */
+    service?: string,
+    /** Use `libraryDebugMode` to log into the terminal what we are sending to the Mizu server on each request/response */
+    libraryDebugMode?: boolean,
+  ) => {
+    // NOTE - Polyfill is probably not necessary for Cloudflare workers, but could be good for vercel envs
+    //         https://github.com/highlight/highlight/pull/6480
+    polyfillWaitUntil(ctx);
 
-		const teardownFunctions: Array<() => void> = [];
+    const teardownFunctions: Array<() => void> = [];
 
-		// TODO - (future) Take the traceId from headers but then fall back to uuid here?
-		const traceId = generateUUID();
+    // TODO - (future) Take the traceId from headers but then fall back to uuid here?
+    const traceId = generateUUID();
 
-		// We monkeypatch `console.*` methods because it's the only way to send consumable logs locally without setting up an otel colletor
-		for (const level of RECORDED_CONSOLE_METHODS) {
-			const originalConsoleMethod = console[level];
-			// HACK - We need to expose a teardown function after requests terminate, so that we can undo our monkeypatching
-			//        Otherwise, each successive request ends up monkeypatching `console.*`, using the previously monkeypatched version!!!
-			teardownFunctions.push(() => {
-				console[level] = originalConsoleMethod;
-			});
+    // We monkeypatch `console.*` methods because it's the only way to send consumable logs locally without setting up an otel colletor
+    for (const level of RECORDED_CONSOLE_METHODS) {
+      const originalConsoleMethod = console[level];
+      // HACK - We need to expose a teardown function after requests terminate, so that we can undo our monkeypatching
+      //        Otherwise, each successive request ends up monkeypatching `console.*`, using the previously monkeypatched version!!!
+      teardownFunctions.push(() => {
+        console[level] = originalConsoleMethod;
+      });
 
-			// TODO - Fix type of `originalMessage`, since devs could really put anything in there...
-			console[level] = (
-				originalMessage: string | Error | NeonDbError,
-				...args: unknown[]
-			) => {
-				const timestamp = new Date().toISOString();
+      // TODO - Fix type of `originalMessage`, since devs could really put anything in there...
+      console[level] = (
+        originalMessage: string | Error | NeonDbError,
+        ...args: unknown[]
+      ) => {
+        const timestamp = new Date().toISOString();
 
-				const callerLocation = extractCallerLocation(
-					(new Error().stack ?? "").split("\n")[2],
-				);
+        const callerLocation = extractCallerLocation(
+          (new Error().stack ?? "").split("\n")[2],
+        );
 
-				let message = originalMessage;
-				if (typeof message !== "string" && message.name === "NeonDbError") {
-					message = JSON.stringify(neonDbErrorToJson(message as NeonDbError));
-				}
-				if (message instanceof Error) {
-					message = JSON.stringify(errorToJson(message));
-				}
+        let message = originalMessage;
+        if (typeof message !== "string" && message.name === "NeonDbError") {
+          message = JSON.stringify(neonDbErrorToJson(message as NeonDbError));
+        }
+        if (message instanceof Error) {
+          message = JSON.stringify(errorToJson(message));
+        }
 
-				const payload = {
-					level,
-					traceId,
-					service,
-					message,
-					args,
-					callerLocation,
-					timestamp,
-				};
-				ctx.waitUntil(
-					fetch(mizuEndpoint, {
-						method: "POST",
-						headers: {
-							"Content-Type": "application/json",
-						},
-						body: JSON.stringify(payload),
-					}),
-				);
-				const applyArgs = args?.length ? [message, ...args] : [message];
-				if (!libraryDebugMode && shouldPrettifyMizuLog(applyArgs)) {
-					// HACK - Try parsing the message as json and extracting all the fields we care about logging prettily
-					tryPrettyPrintLoggerLog(originalConsoleMethod, message);
-				} else {
-					originalConsoleMethod.apply(originalConsoleMethod, applyArgs);
-				}
-			};
-		}
+        const payload = {
+          level,
+          traceId,
+          service,
+          message,
+          args,
+          callerLocation,
+          timestamp,
+        };
+        ctx.waitUntil(
+          fetch(mizuEndpoint, {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(payload),
+          }),
+        );
+        const applyArgs = args?.length ? [message, ...args] : [message];
+        if (!libraryDebugMode && shouldPrettifyMizuLog(applyArgs)) {
+          // HACK - Try parsing the message as json and extracting all the fields we care about logging prettily
+          tryPrettyPrintLoggerLog(originalConsoleMethod, message);
+        } else {
+          originalConsoleMethod.apply(originalConsoleMethod, applyArgs);
+        }
+      };
+    }
 
-		return () => {
-			for (const teardownFunction of teardownFunctions) {
-				teardownFunction();
-			}
-		};
-	},
+    return () => {
+      for (const teardownFunction of teardownFunctions) {
+        teardownFunction();
+      }
+    };
+  },
 };
--- client-library/src/request-logger.ts
+++ client-library/src/request-logger.ts
@@ -3,11 +3,11 @@ import { getPath } from "hono/utils/url";
 import type { Context } from "hono";
 
 export const RECORDED_CONSOLE_METHODS = [
-	"debug",
-	"error",
-	"info",
-	"log",
-	"warn",
+  "debug",
+  "error",
+  "info",
+  "log",
+  "warn",
 ] as const;
 
 import { PRETTIFY_MIZU_LOGGER_LOG, type PrintFunc } from "./utils";
@@ -15,196 +15,196 @@ import { PRETTIFY_MIZU_LOGGER_LOG, type PrintFunc } from "./utils";
 // === LOGGER FUNCTION === //
 
 function logReq(
-	fn: PrintFunc,
-	method: string,
-	headers: Record<string, string>,
-	path: string,
-	env: Record<string, string>,
-	params: unknown,
-	query: Record<string, string>,
-	// Originating file of the request handler
-	file?: string | null,
+  fn: PrintFunc,
+  method: string,
+  headers: Record<string, string>,
+  path: string,
+  env: Record<string, string>,
+  params: unknown,
+  query: Record<string, string>,
+  // Originating file of the request handler
+  file?: string | null,
 ) {
-	const out = {
-		lifecycle: "request",
-		method,
-		headers,
-		path,
-		env,
-		params,
-		query,
-		file,
-	};
+  const out = {
+    lifecycle: "request",
+    method,
+    headers,
+    path,
+    env,
+    params,
+    query,
+    file,
+  };
 
-	// NOTE - We log a symbol that allows us to hide the log in the terminal UI by default and print something prettier
-	fn(JSON.stringify(out), PRETTIFY_MIZU_LOGGER_LOG);
+  // NOTE - We log a symbol that allows us to hide the log in the terminal UI by default and print something prettier
+  fn(JSON.stringify(out), PRETTIFY_MIZU_LOGGER_LOG);
 }
 
 export const logger = (
-	// HACK - Use print functions that just invoke console.log from outer context
-	//        We do this since console.log is monkeypatched in the mizu middleware, so we can't default to just `console.log`
-	fn: PrintFunc = (message: string, ...args: unknown[]) =>
-		console.log(message, ...args),
-	errFn: PrintFunc = (message: string, ...args: unknown[]) =>
-		console.error(message, ...args),
+  // HACK - Use print functions that just invoke console.log from outer context
+  //        We do this since console.log is monkeypatched in the mizu middleware, so we can't default to just `console.log`
+  fn: PrintFunc = (message: string, ...args: unknown[]) =>
+    console.log(message, ...args),
+  errFn: PrintFunc = (message: string, ...args: unknown[]) =>
+    console.error(message, ...args),
 ): MiddlewareHandler => {
-	return (c: Context, next: () => Promise<void>) =>
-		logRequest(c, next, { fn, errFn });
+  return (c: Context, next: () => Promise<void>) =>
+    logRequest(c, next, { fn, errFn });
 };
 
 export async function logRequest(
-	c: Context,
-	next: () => Promise<void>,
-	options?: {
-		fn: PrintFunc;
-		errFn: PrintFunc;
-	},
+  c: Context,
+  next: () => Promise<void>,
+  options?: {
+    fn: PrintFunc;
+    errFn: PrintFunc;
+  },
 ): Promise<void> {
-	const {
-		fn = (message: string, ...args: unknown[]) => console.log(message, ...args),
-		errFn = (message: string, ...args: unknown[]) =>
-			console.error(message, ...args),
-	} = options || {};
-	// Use a stack trace to get the originating file
-	const stack = new Error().stack;
-	const file = getFileFromStackTrace(stack ?? "");
+  const {
+    fn = (message: string, ...args: unknown[]) => console.log(message, ...args),
+    errFn = (message: string, ...args: unknown[]) =>
+      console.error(message, ...args),
+  } = options || {};
+  // Use a stack trace to get the originating file
+  const stack = new Error().stack;
+  const file = getFileFromStackTrace(stack ?? "");
 
-	// Get basic request data
-	const { method } = c.req;
-	const path = getPath(c.req.raw);
+  // Get basic request data
+  const { method } = c.req;
+  const path = getPath(c.req.raw);
 
-	// Copy request headers into plain object
-	const reqHeaders: Record<string, string> = {};
-	c.req.raw.headers.forEach((value, key) => {
-		reqHeaders[key] = value;
-	});
+  // Copy request headers into plain object
+  const reqHeaders: Record<string, string> = {};
+  c.req.raw.headers.forEach((value, key) => {
+    reqHeaders[key] = value;
+  });
 
-	const requestQueryStringParameters = c.req.query();
-	logReq(
-		fn,
-		method,
-		reqHeaders,
-		path,
-		c.env,
-		c.req.param(),
-		requestQueryStringParameters,
-		file,
-	);
+  const requestQueryStringParameters = c.req.query();
+  logReq(
+    fn,
+    method,
+    reqHeaders,
+    path,
+    c.env,
+    c.req.param(),
+    requestQueryStringParameters,
+    file,
+  );
 
-	const start = Date.now();
+  const start = Date.now();
 
-	await next();
+  await next();
 
-	const elapsed = time(start);
+  const elapsed = time(start);
 
-	const matchedPathPattern = c.req.routePath;
-	// HACK - We know we will get a matched route, so coerce the type of `matchedRoute` to RouterRoute
-	const matchedRoute: RouterRoute = c.req.matchedRoutes.find((route) => {
-		return route.path === c.req.routePath;
-	}) as RouterRoute;
+  const matchedPathPattern = c.req.routePath;
+  // HACK - We know we will get a matched route, so coerce the type of `matchedRoute` to RouterRoute
+  const matchedRoute: RouterRoute = c.req.matchedRoutes.find((route) => {
+    return route.path === c.req.routePath;
+  }) as RouterRoute;
 
-	const matchedPathHandler = matchedRoute?.handler;
+  const matchedPathHandler = matchedRoute?.handler;
 
-	const handlerType = matchedPathHandler.length < 2 ? "handler" : "middleware";
+  const handlerType = matchedPathHandler.length < 2 ? "handler" : "middleware";
 
-	// Copy headers into a plain object
-	const resHeaders: Record<string, string> = {};
-	c.res.headers.forEach((value, key) => {
-		resHeaders[key] = value;
-	});
+  // Copy headers into a plain object
+  const resHeaders: Record<string, string> = {};
+  c.res.headers.forEach((value, key) => {
+    resHeaders[key] = value;
+  });
 
-	// Clone the response so the original isn't affected when we read the body
-	const clonedResponse = c.res.clone();
-	let body: string;
-	try {
-		// TODO - Read based off of content-type header
-		body = await clonedResponse.text();
-	} catch (error) {
-		// TODO - Check when this fails
-		console.error("Error reading response body:", error);
-		body = "__COULD_NOT_PARSE_BODY__";
-	}
+  // Clone the response so the original isn't affected when we read the body
+  const clonedResponse = c.res.clone();
+  let body: string;
+  try {
+    // TODO - Read based off of content-type header
+    body = await clonedResponse.text();
+  } catch (error) {
+    // TODO - Check when this fails
+    console.error("Error reading response body:", error);
+    body = "__COULD_NOT_PARSE_BODY__";
+  }
 
-	// NOTE - Use errFn (console.error) if the status is 4xx or 5xx
-	const loggerFn = c.res.status >= 400 ? errFn : fn;
+  // NOTE - Use errFn (console.error) if the status is 4xx or 5xx
+  const loggerFn = c.res.status >= 400 ? errFn : fn;
 
-	logRes(
-		loggerFn,
-		method,
-		path,
-		matchedPathPattern,
-		matchedPathHandler?.toString(),
-		handlerType,
-		c.res.status,
-		resHeaders,
-		body,
-		elapsed,
-	);
+  logRes(
+    loggerFn,
+    method,
+    path,
+    matchedPathPattern,
+    matchedPathHandler?.toString(),
+    handlerType,
+    c.res.status,
+    resHeaders,
+    body,
+    elapsed,
+  );
 }
 
 function getFileFromStackTrace(stack: string) {
-	// Regular expression to match the pattern "file://path:line:column"
-	const regex = /file:\/\/(\/[\w\-. /]+):(\d+):(\d+)/;
+  // Regular expression to match the pattern "file://path:line:column"
+  const regex = /file:\/\/(\/[\w\-. /]+):(\d+):(\d+)/;
 
-	// Attempt to match the regex pattern against the provided stack trace
-	const match = stack.match(regex);
+  // Attempt to match the regex pattern against the provided stack trace
+  const match = stack.match(regex);
 
-	if (match) {
-		// if (stack.includes("neon")) {
-		//   debugger
-		// }
-		// Extract the file path, line, and column from the regex match
-		const [, source] = match;
-		return source;
-	}
+  if (match) {
+    // if (stack.includes("neon")) {
+    //   debugger
+    // }
+    // Extract the file path, line, and column from the regex match
+    const [, source] = match;
+    return source;
+  }
 
-	// Return null or throw an error if no match is found
-	return null;
+  // Return null or throw an error if no match is found
+  return null;
 }
 
 function logRes(
-	fn: PrintFunc,
-	method: string,
-	path: string,
-	matchedPathPattern?: string,
-	matchedPathHandler?: string,
-	handlerType?: string,
-	status = 0,
-	headers?: Record<string, string>,
-	body?: string,
-	elapsed?: string,
+  fn: PrintFunc,
+  method: string,
+  path: string,
+  matchedPathPattern?: string,
+  matchedPathHandler?: string,
+  handlerType?: string,
+  status = 0,
+  headers?: Record<string, string>,
+  body?: string,
+  elapsed?: string,
 ) {
-	const out = {
-		method,
-		lifecycle: "response",
-		path,
-		route: matchedPathPattern,
-		handler: matchedPathHandler,
-		handlerType, // Unsure if this is useful... or how
-		status: status?.toString(), // HACK - For compatibiltiy with mizu UI
-		headers,
-		body,
-		elapsed,
-	};
+  const out = {
+    method,
+    lifecycle: "response",
+    path,
+    route: matchedPathPattern,
+    handler: matchedPathHandler,
+    handlerType, // Unsure if this is useful... or how
+    status: status?.toString(), // HACK - For compatibiltiy with mizu UI
+    headers,
+    body,
+    elapsed,
+  };
 
-	fn(JSON.stringify(out), PRETTIFY_MIZU_LOGGER_LOG);
+  fn(JSON.stringify(out), PRETTIFY_MIZU_LOGGER_LOG);
 }
 
 const humanize = (times: string[]) => {
-	const [delimiter, separator] = [",", "."];
+  const [delimiter, separator] = [",", "."];
 
-	const orderTimes = times.map((v) =>
-		// biome-ignore lint/style/useTemplate: copied from hono source
-		v.replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1" + delimiter),
-	);
+  const orderTimes = times.map((v) =>
+    // biome-ignore lint/style/useTemplate: copied from hono source
+    v.replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1" + delimiter),
+  );
 
-	return orderTimes.join(separator);
+  return orderTimes.join(separator);
 };
 
 const time = (start: number) => {
-	const delta = Date.now() - start;
-	return humanize([
-		// biome-ignore lint/style/useTemplate: copied from hono source
-		delta < 1000 ? delta + "ms" : Math.round(delta / 1000) + "s",
-	]);
+  const delta = Date.now() - start;
+  return humanize([
+    // biome-ignore lint/style/useTemplate: copied from hono source
+    delta < 1000 ? delta + "ms" : Math.round(delta / 1000) + "s",
+  ]);
 };
