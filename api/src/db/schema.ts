281665f4ed2761db81ccf664f3d68fc90162cf9a
Laurynas Keturakis <laurynas.keturakis@gmail.com>
2024-06-04 18:06:24 +0200

    add schema for all github issues

--- api/src/db/schema.ts
+++ api/src/db/schema.ts
@@ -1,6 +1,15 @@
 import { sql } from "drizzle-orm";
 import { text, integer, sqliteTable } from "drizzle-orm/sqlite-core";
-import { GithubIssue } from "@/routes/issues";
+import type { Endpoints } from "@octokit/types";
+import { createInsertSchema, createSelectSchema } from "drizzle-zod";
+import { z } from "zod";
+
+// TYPES
+// These are helper types from Octokit, which are used in the `githubIssues` table
+type OctokitResponseGithubIssues =
+  Endpoints["GET /repos/{owner}/{repo}/issues"]["response"];
+
+type OctokitGithubIssue = OctokitResponseGithubIssues["data"][number];
 
 // HELPFUL: https://orm.drizzle.team/docs/column-types/sqlite
 export const mizuLogs = sqliteTable("mizu_logs", {
@@ -23,89 +32,49 @@ export const mizuLogs = sqliteTable("mizu_logs", {
 
 export const githubIssues = sqliteTable("github_issues", {
   id: integer("id", { mode: "number" }).primaryKey(),
+  owner: text("owner", { mode: "text" }).notNull(),
+  repo: text("repo", { mode: "text" }).notNull(),
   url: text("url", { mode: "text" }).notNull(),
-  title: text("title", { mode: "text" }).notNull(),
+  title: text("title", { mode: "text" }).notNull().default(""),
   body: text("body", { mode: "text" }),
-  state: text("state", { mode: "text" }).notNull(),
-  labels: text("labels", { mode: "json" })
-    .$type<GithubIssue["labels"][number]>()
-    .notNull(),
-  milestone: text("milestone", { mode: "json" }).$type<
-    GithubIssue["milestone"]
+  state: text("state", {
+    mode: "text",
+    enum: ["open", "closed"],
+  }).notNull(),
+  labels: text("labels", { mode: "json" }).$type<
+    OctokitGithubIssue["labels"]
   >(),
-	createdAt: text("created_at").notNull(),
-	updatedAt: text("updated_at").notNull(),
-	closedAt: text("closed_at"),
+  createdAt: text("created_at"),
+  updatedAt: text("updated_at"),
+  closedAt: text("closed_at"),
 });
 
-type PR = GithubIssue["pull_request"];
-type closed_at = GithubIssue["closed_at"];
+// GitHub might return an object or a string for the labels field so I'm just
+// normalizing it all to a string array
+const normalizeLabels = {
+  labels: z.array(
+    z.union([
+      z.string().transform((labelString) => labelString),
+      z
+        .object({ name: z.string() })
+        .transform((labelObject) => labelObject.name),
+    ]),
+  ),
+};
 
-export type RecordedGithubIssue = typeof githubIssues.$inferSelect;
+export const newGithubIssueSchema = createInsertSchema(
+  githubIssues,
+  normalizeLabels,
+);
+export const githubIssueSchema = createSelectSchema(
+  githubIssues,
+  normalizeLabels,
+);
 
-// Github Issues response
-// [
-//   {
-//     "id": 1,
-//     "node_id": "MDU6SXNzdWUx",
-//     "url": "https://api.github.com/repos/octocat/Hello-World/issues/1347",
-//     "repository_url": "https://api.github.com/repos/octocat/Hello-World",
-//     "labels_url": "https://api.github.com/repos/octocat/Hello-World/issues/1347/labels{/name}",
-//     "comments_url": "https://api.github.com/repos/octocat/Hello-World/issues/1347/comments",
-//     "events_url": "https://api.github.com/repos/octocat/Hello-World/issues/1347/events",
-//     "html_url": "https://github.com/octocat/Hello-World/issues/1347",
-//     "number": 1347,
-//     "state": "open",
-//     "title": "Found a bug",
-//     "body": "I'm having a problem with this.",
-//     "user": {
-//     },
-//     "labels": [
-//       {
-//       }
-//     ],
-//     "assignee": {
-//     },
-//     "assignees": [
-//     ],
-//     "milestone": {
-//     },
-//     "locked": true,
-//     "active_lock_reason": "too heated",
-//     "comments": 0,
-//     "pull_request": {
-//       "url": "https://api.github.com/repos/octocat/Hello-World/pulls/1347",
-//       "html_url": "https://github.com/octocat/Hello-World/pull/1347",
-//       "diff_url": "https://github.com/octocat/Hello-World/pull/1347.diff",
-//       "patch_url": "https://github.com/octocat/Hello-World/pull/1347.patch"
-//     },
-//     "closed_at": null,
-//     "created_at": "2011-04-22T13:33:48Z",
-//     "updated_at": "2011-04-22T13:33:48Z",
-//     "closed_by": {
-//       "login": "octocat",
-//       "id": 1,
-//       "node_id": "MDQ6VXNlcjE=",
-//       "avatar_url": "https://github.com/images/error/octocat_happy.gif",
-//       "gravatar_id": "",
-//       "url": "https://api.github.com/users/octocat",
-//       "html_url": "https://github.com/octocat",
-//       "followers_url": "https://api.github.com/users/octocat/followers",
-//       "following_url": "https://api.github.com/users/octocat/following{/other_user}",
-//       "gists_url": "https://api.github.com/users/octocat/gists{/gist_id}",
-//       "starred_url": "https://api.github.com/users/octocat/starred{/owner}{/repo}",
-//       "subscriptions_url": "https://api.github.com/users/octocat/subscriptions",
-//       "organizations_url": "https://api.github.com/users/octocat/orgs",
-//       "repos_url": "https://api.github.com/users/octocat/repos",
-//       "events_url": "https://api.github.com/users/octocat/events{/privacy}",
-//       "received_events_url": "https://api.github.com/users/octocat/received_events",
-//       "type": "User",
-//       "site_admin": false
-//     },
-//     "author_association": "COLLABORATOR",
-//     "state_reason": "completed"
-//   }
-// ]
+// When you create an issue record
+export type NewGitHubIssue = z.infer<typeof newGithubIssueSchema>;
+// When you select an issue record
+export type GitHubIssue = z.infer<typeof githubIssueSchema>;
 
 // When you select a record
 export type MizuLog = typeof mizuLogs.$inferSelect; // return type when queried
