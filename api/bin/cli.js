6d2c9ae4d4cb26706b5028c8461ac843fa220a46
Brett Beutell <brbeut@gmail.com>
2024-06-05 15:54:57 +0200

    Hackily automigrate

--- api/bin/cli.js
+++ api/bin/cli.js
@@ -6,22 +6,31 @@ import path from "node:path";
 const args = process.argv.slice(2);
 const script = args[0];
 
+// HACK - if no script is specified, migrate then open studio
+const scripts = !script ? ["migrate", "studio"] : [script];
+
 const validScripts = {
   migrate: "dist/migrate.js",
   studio: "dist/src/index.node.js",
 };
 
-if (!validScripts[script]) {
-  console.error(
-    `Invalid script "${script}". Valid scripts are: ${Object.keys(validScripts).join(", ")}`,
-  );
-  process.exit(1);
+scripts.forEach(runScript);
+
+function runScript(scriptName) {
+  const scriptPath = validScripts[scriptName];
+  if (!scriptPath) {
+    console.error(
+      `Invalid script "${scriptName}". Valid scripts are: ${Object.keys(validScripts).join(", ")}`,
+    );
+    process.exit(1);
+  }
+
+  // Get the root directory of this script's project
+  const scriptDir = path.resolve(__dirname, "../");
+
+  // Construct the command to run the appropriate script in the `dist` folder
+  const command = `node ${path.join(scriptDir, scriptPath)}`;
+
+  execSync(command, { stdio: "inherit" });
 }
 
-// Get the root directory of this script's project
-const scriptDir = path.resolve(__dirname, "../");
-
-// Construct the command to run the appropriate script in the `dist` folder
-const command = `node ${path.join(scriptDir, validScripts[script])}`;
-
-execSync(command, { stdio: "inherit" });
