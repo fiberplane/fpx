a7405b0776afdc06db99f64cc2a05bcfeb775998
Jacco Flenter <flenter@users.noreply.github.com>
2024-06-05 13:38:28 +0200

    Simplify biome.jsonc files
    And format code

--- api/src/index.node.ts
+++ api/src/index.node.ts
@@ -1,5 +1,5 @@
-import { serve } from "@hono/node-server";
 import { createServer } from "node:http";
+import { serve } from "@hono/node-server";
 import { config } from "dotenv";
 
 import { type WebSocket, WebSocketServer } from "ws";
@@ -12,30 +12,30 @@ const app = createApp(wsConnections);
 
 // health check
 app.get("/", async (c) => {
-	return c.text("Hello Node.js Hono");
+  return c.text("Hello Node.js Hono");
 });
 
 const port = 8788;
 const server = serve({
-	fetch: app.fetch,
-	port,
-	createServer,
+  fetch: app.fetch,
+  port,
+  createServer,
 }) as ReturnType<typeof createServer>;
 console.log(`Server is running: http://localhost:${port}`);
 
 const wss = new WebSocketServer({ server });
 
 wss.on("connection", (ws) => {
-	console.log("WebSocket connection established", ws.OPEN);
-	wsConnections.add(ws);
+  console.log("WebSocket connection established", ws.OPEN);
+  wsConnections.add(ws);
 
-	ws.on("ping", () => {
-		console.log("ping");
-		ws.send("pong");
-	});
-	ws.on("error", console.error);
-	ws.on("close", (code) => {
-		wsConnections.delete(ws);
-		console.log("WebSocket connection closed", code);
-	});
+  ws.on("ping", () => {
+    console.log("ping");
+    ws.send("pong");
+  });
+  ws.on("error", console.error);
+  ws.on("close", (code) => {
+    wsConnections.delete(ws);
+    console.log("WebSocket connection closed", code);
+  });
 });
