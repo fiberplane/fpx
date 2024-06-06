eb68a83cd3f0b058b579909f81b3453a1f422da8
Jacco Flenter <flenter@users.noreply.github.com>
2024-06-06 22:26:40 +0200

    Fix formatting

--- api/src/db/schema.ts
+++ api/src/db/schema.ts
@@ -23,7 +23,7 @@ export const mizuLogs = sqliteTable("mizu_logs", {
   args: text("args", { mode: "json" }), // NOTE - Should only be present iff message is a string
   callerLocation: text("caller_location", { mode: "json" }),
   createdAt: text("created_at").notNull().default(sql`(CURRENT_TIMESTAMP)`),
-  updatedAt: text("updated_at") .notNull().default(sql`(CURRENT_TIMESTAMP)`),
+  updatedAt: text("updated_at").notNull().default(sql`(CURRENT_TIMESTAMP)`),
   matchingIssues: text("matching_issues", { mode: "json" }).$type<
     number[] | null
   >(),
