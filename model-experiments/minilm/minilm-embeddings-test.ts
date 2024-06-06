caddb4ecf69526e7c095c7d9354133e5f83bba06
Laurynas Keturakis <laurynas.keturakis@gmail.com>
2024-06-06 14:59:59 +0200

    create a minilm embedding script and smoke test

---
+++ model-experiments/minilm/minilm-embeddings-test.ts
@@ -0,0 +1,48 @@
+// This is just a quick smoke test to see if the embeddings have actually been created
+// and if they are retrievable
+
+import { restoreFromFile } from "@orama/plugin-data-persistence/server";
+import { search } from "@orama/orama";
+import { Endpoints } from "@octokit/types";
+import fs from "node:fs";
+import {
+  FeatureExtractionPipelineOptions,
+  pipeline,
+} from "@xenova/transformers";
+
+type GithubIssue =
+  Endpoints["GET /repos/{owner}/{repo}/issues"]["response"]["data"][number];
+
+const miniLmEmbedder = await pipeline(
+  "feature-extraction",
+  "Xenova/all-MiniLM-L6-v2",
+);
+
+const optionsMiniLmEmbedder: FeatureExtractionPipelineOptions = {
+  pooling: "mean",
+  normalize: true,
+};
+
+const db = await restoreFromFile("binary", "minilm-embeddings.msp");
+
+const issuesHonoJSMiddleware: GithubIssue[] = JSON.parse(
+  fs.readFileSync("issues/honojs-middleware.json", "utf8").toString(),
+);
+
+const random5Indexes = [4, 25, 52, 100, 120];
+
+for (const index of random5Indexes) {
+  const issue = issuesHonoJSMiddleware[index];
+  const embedding = (await miniLmEmbedder(issue.body, optionsMiniLmEmbedder))
+    .tolist()
+    .flat();
+  const similarIssues = await search(db, {
+    mode: "vector",
+    vector: {
+      value: embedding,
+      property: "vector",
+    },
+  });
+  console.log(`Similar issues for issue ${issue.title}:`);
+  console.log(similarIssues);
+}
---
+++ model-experiments/minilm/minilm-embeddings.ts
@@ -0,0 +1,87 @@
+import { create, insert, insertMultiple, search } from "@orama/orama";
+import {
+  pipeline,
+  env,
+  type FeatureExtractionPipelineOptions,
+} from "@xenova/transformers";
+import {
+  restoreFromFile,
+  persistToFile,
+} from "@orama/plugin-data-persistence/server";
+import fs from "node:fs/promises";
+
+const files = [
+  "drizzle-team-drizzle-orm.json",
+  "drizzle-team-drizzle-kit-mirror.json",
+  "honojs-hono.json",
+  "honojs-middleware.json",
+  "neondatabase-serverless.json",
+];
+
+const miniLmEmbedder = await pipeline(
+  "feature-extraction",
+  "Xenova/all-MiniLM-L6-v2",
+);
+
+const optionsMiniLmEmbedder: FeatureExtractionPipelineOptions = {
+  pooling: "mean",
+  normalize: true,
+};
+
+const orama = await create({
+  schema: {
+    title: "string",
+    body: "string",
+    state: "string",
+    labels: "string",
+    createdAt: "string",
+    updatedAt: "string",
+    closedAt: "string",
+    vector: "vector[384]",
+  },
+});
+
+let ALL_ISSUES = [];
+
+async function processIssuesFile(file: string) {
+  try {
+    console.log(`Processing file ${file}`);
+    const fileBuffer = await fs.readFile(`issues/${file}`);
+    const fileData = JSON.parse(fileBuffer.toString());
+    console.log(`Read ${fileData.length} issues from file ${file}`);
+    console.log("Creating embeddings...");
+		const issues = await Promise.all(fileData.map(async (issue) => ({
+			title: issue.title,
+			body: issue.body ?? "",
+			state: issue.state,
+			createdAt: issue.created_at,
+			updatedAt: issue.updated_at,
+			closedAt: issue.closed_at ?? "",
+			vector: (
+				await miniLmEmbedder(
+					`${issue.title} ${issue.body}`,
+					optionsMiniLmEmbedder,
+				)
+			)
+			.tolist()
+			.flat(),
+		})));
+    console.log(`Created embeddings for ${issues.length} issues`);
+    ALL_ISSUES = ALL_ISSUES.concat(issues);
+  } catch (error) {
+    console.error(`Error processing file ${file}`, error);
+    process.exit(1);
+  }
+}
+
+try {
+  console.log("Processing files...");
+  await Promise.all(files.map(processIssuesFile));
+  console.log("Inserting embeddings...");
+  await insertMultiple(orama, ALL_ISSUES);
+  console.log(`Inserted embeddings for ${ALL_ISSUES.length} issues`);
+  await persistToFile(orama, "binary", "minilm-embeddings.msp");
+} catch (error) {
+  console.error(error);
+  process.exit(1);
+}
