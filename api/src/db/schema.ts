3fb64eb3d744b5a2cf716d1e0deb34b7297269a3
Laurynas Keturakis <laurynas.keturakis@gmail.com>
2024-06-06 20:47:57 +0200

    update db schema to include type for the issues/PRs

--- api/src/db/schema.ts
+++ api/src/db/schema.ts
@@ -22,8 +22,12 @@ export const mizuLogs = sqliteTable("mizu_logs", {
   ignored: integer("ignored", { mode: "boolean" }).default(false),
   args: text("args", { mode: "json" }), // NOTE - Should only be present iff message is a string
   callerLocation: text("caller_location", { mode: "json" }),
-  createdAt: text("created_at").notNull().default(sql`(CURRENT_TIMESTAMP)`),
-  updatedAt: text("updated_at").notNull().default(sql`(CURRENT_TIMESTAMP)`),
+  createdAt: text("created_at")
+    .notNull()
+    .default(sql`(CURRENT_TIMESTAMP)`),
+  updatedAt: text("updated_at")
+    .notNull()
+    .default(sql`(CURRENT_TIMESTAMP)`),
   matchingIssues: text("matching_issues", { mode: "json" }).$type<
     number[] | null
   >(),
@@ -40,6 +44,7 @@ export const githubIssues = sqliteTable("github_issues", {
     mode: "text",
     enum: ["open", "closed"],
   }).notNull(),
+  type: text("type", { enum: ["issue", "pull_request"] }).notNull(),
   labels: text("labels", { mode: "json" }).$type<
     OctokitGithubIssue["labels"]
   >(),
@@ -47,19 +52,6 @@ export const githubIssues = sqliteTable("github_issues", {
   updatedAt: text("updated_at"),
   closedAt: text("closed_at"),
 });
-
-// GitHub might return an object or a string for the labels field so I'm just
-// normalizing it all to a string array
-const normalizeLabels = {
-  labels: z.array(
-    z.union([
-      z.string().transform((labelString) => labelString),
-      z
-        .object({ name: z.string() })
-        .transform((labelObject) => labelObject.name),
-    ]),
-  ),
-};
 
 // Eventually this might be a separate table? Either way for now just keeping schemas
 // in one place
@@ -74,6 +66,19 @@ export const dependencySchema = z.object({
 });
 
 export type Dependency = z.infer<typeof dependencySchema>;
+
+// GitHub might return an object or a string for the labels field so I'm just
+// normalizing it all to a string array
+const normalizeLabels = {
+  labels: z.array(
+    z.union([
+      z.string().transform((labelString) => labelString),
+      z
+        .object({ name: z.string() })
+        .transform((labelObject) => labelObject.name),
+    ]),
+  ),
+};
 
 export const newGithubIssueSchema = createInsertSchema(
   githubIssues,
