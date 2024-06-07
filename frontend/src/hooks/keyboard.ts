7008b7045c77a48971c7933b1cb42d49e1e206d1
Laurynas Keturakis <laurynas.keturakis@gmail.com>
2024-06-07 19:27:11 +0200

    update with fixes so that all instances of useKeySequence registered properly

--- frontend/src/hooks/keyboard.ts
+++ frontend/src/hooks/keyboard.ts
@@ -1,5 +1,14 @@
 import { useCallback, useEffect, useRef, useState } from "react";
 
+/**
+ * Define a sequence of keys to be pressed in order to trigger a callback. Note that it should be used only for sequences, not chords. TODO: create a hook for chords.
+ *
+ * @param sequence - An array of keys to be pressed in order to trigger the callback.
+ * @param callback - The callback to be executed when the sequence of keys is pressed.
+ * @param timeout - The timeout in milliseconds to wait before executing the callback. Default is 2000.
+ * @returns keySequence - An array of the keys that were pressed in order to trigger the callback. This is useful for debugging purposes.
+ *
+ */
 export function useKeySequence(
   sequence: string[],
   callback: () => void,
@@ -7,24 +16,31 @@ export function useKeySequence(
 ) {
   const [keySequence, setKeySequence] = useState<string[]>([]);
   const timeoutRef = useRef<number | undefined>(undefined);
-
-  const handleKeyPress = useCallback(
-    (event: KeyboardEvent) => {
-      setKeySequence((prevKeySequence) => {
-        const updatedSequence = [...prevKeySequence, event.key].slice(
-          -sequence.length,
-        );
-
-        if (updatedSequence.join("") === sequence.join("")) {
-          callback();
-          return [];
-        }
-
-        return updatedSequence;
-      });
-    },
-    [sequence, callback],
+  const sequenceRef = useRef<{ sequence: string[]; callback: () => void }[]>(
+    [],
   );
+
+	// this is so we can call the useKeySequence hook multiple times
+	// an alternative would be to have a `useKeySequences([{sequence, callback}])` hook
+	// to register all in one
+  useEffect(() => {
+    sequenceRef.current.push({ sequence, callback });
+  }, [sequence, callback]);
+
+  const handleKeyPress = useCallback((event: KeyboardEvent) => {
+    setKeySequence((prevKeySequence) => {
+      const updatedSequence = [...prevKeySequence, event.key].slice(
+        -sequence.length,
+      );
+
+      if (updatedSequence.join("") === sequence.join("")) {
+        callback();
+        return [];
+      }
+
+      return updatedSequence;
+    });
+  }, []);
 
   useEffect(() => {
     const resetSequence = () => setKeySequence([]);
