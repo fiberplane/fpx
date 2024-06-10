7181f321c52619eab70c981b382c8b2060c791a3
Laurynas Keturakis <laurynas.keturakis@gmail.com>
2024-06-11 00:11:39 +0200

    imports order

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,9 @@
 import { QueryClientProvider, queryClient } from "@/queries";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Layout from "./Layout";
+import { ThemeProvider } from "./components/theme-provider";
 import { RequestDetailsPage } from "./pages/RequestDetailsPage/RequestDetailsPage";
 import { RequestsPage } from "./pages/RequestsPage/Requests";
-import { ThemeProvider } from "./components/theme-provider";
 
 export function App() {
   return (
