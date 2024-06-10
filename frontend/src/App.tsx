276c4630701ecdf6d385ce696693f12a80503e33
Laurynas Keturakis <laurynas.keturakis@gmail.com>
2024-06-10 23:40:14 +0200

    throw on the initial scaffold copy pasted from shadcn guides

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,21 +3,24 @@ import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Layout from "./Layout";
 import { RequestDetailsPage } from "./pages/RequestDetailsPage/RequestDetailsPage";
 import { RequestsPage } from "./pages/RequestsPage/Requests";
+import { ThemeProvider } from "./components/theme-provider";
 
 export function App() {
-  return (
-    <QueryClientProvider client={queryClient}>
-      <Router>
-        <Layout>
-          <Routes>
-            <Route path="/" element={<RequestsPage />} />
-            <Route path="/requests" element={<RequestsPage />} />
-            <Route path="/requests/:traceId" element={<RequestDetailsPage />} />
-          </Routes>
-        </Layout>
-      </Router>
-    </QueryClientProvider>
-  );
+	return (
+		<ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+			<QueryClientProvider client={queryClient}>
+				<Router>
+					<Layout>
+						<Routes>
+							<Route path="/" element={<RequestsPage />} />
+							<Route path="/requests" element={<RequestsPage />} />
+							<Route path="/requests/:traceId" element={<RequestDetailsPage />} />
+						</Routes>
+					</Layout>
+				</Router>
+			</QueryClientProvider>
+		</ThemeProvider>
+	);
 }
 
 export default App;
---
+++ frontend/src/hooks/theme.ts
@@ -0,0 +1,11 @@
+import { ThemeProviderContext } from "@/components/theme-provider"
+import { useContext } from "react"
+
+export const useTheme = () => {
+	const context = useContext(ThemeProviderContext)
+
+	if (context === undefined)
+		throw new Error("useTheme must be used within a ThemeProvider")
+
+	return context
+}
