88f8e89e4f94ea9aa4f2c1569a0b287b5f8482cb
Brett Beutell <brbeut@gmail.com>
2024-06-17 14:59:55 +0200

    Fix linting error with button variants

--- frontend/src/components/ui/pagination.tsx
+++ frontend/src/components/ui/pagination.tsx
@@ -5,8 +5,9 @@ import {
 } from "@radix-ui/react-icons";
 import * as React from "react";
 
-import { ButtonProps, buttonVariants } from "@/components/ui/button";
+import { ButtonProps } from "@/components/ui/button";
 import { cn } from "@/utils";
+import { buttonVariants } from "./buttonVariants";
 
 const Pagination = ({ className, ...props }: React.ComponentProps<"nav">) => (
   <nav
