de5ffb6ea36498e72e23815a01025a0128501954
Benno van den Berg <bennovandenberg@gmail.com>
2024-06-21 11:07:00 +0200

    Add some simple tracing
    When retrieving a file from a dir_entry, use the _full_ path, not just the name within the dir_entry. Not sure if this is a bug or not

--- fpx/src/api/studio.rs
+++ fpx/src/api/studio.rs
@@ -2,25 +2,28 @@ use axum::extract::Request;
 use axum::response::IntoResponse;
 use http::StatusCode;
 use include_dir::{include_dir, Dir, DirEntry, File};
+use tracing::trace;
 
 static STUDIO_DIST: Dir<'_> = include_dir!("$CARGO_MANIFEST_DIR/../frontend/dist");
 
 /// A simple handler that serves the frontend Studio from STUDIO_DIST.
 pub async fn default_handler(req: Request) -> impl IntoResponse {
+    trace!(uri=?req.uri(), "Serving file from embedded Studio");
+
     let path = req.uri().path().trim_start_matches('/');
 
     // Retrieve the File that according to the path. If it is a directory, see
-    // if there is an index.html file. Always fallback to the index.html in the
-    // root.
+    // if there is an index.html file. Otherwise, always fallback to the
+    // index.html in the root.
     let file: Option<&File> = STUDIO_DIST
         .get_entry(path)
         .and_then(|entry| match entry {
-            DirEntry::Dir(dir_entry) => dir_entry.get_file("index.html"),
+            DirEntry::Dir(dir_entry) => dir_entry.get_file(dir_entry.path().join("index.html")),
             DirEntry::File(file_entry) => Some(file_entry),
         })
         .or_else(|| STUDIO_DIST.get_file("index.html"));
 
-    // Just return 404 if no file was found.
+    // If nothing matches _at all_, then return a 404.
     let Some(file) = file else {
         return StatusCode::NOT_FOUND.into_response();
     };
