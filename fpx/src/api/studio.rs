0644e60ff81bfe4ff6449bcf66896398ef54d9bc
Benno van den Berg <bennovandenberg@gmail.com>
2024-06-21 10:35:37 +0200

    Allow unused fn

--- fpx/src/api/studio.rs
+++ fpx/src/api/studio.rs
@@ -1,63 +1,44 @@
 use axum::extract::Request;
-use axum::response::{IntoResponse, Response};
+use axum::response::IntoResponse;
 use http::StatusCode;
-use include_dir::{include_dir, Dir, DirEntry};
+use include_dir::{include_dir, Dir, DirEntry, File};
 
 static STUDIO_DIST: Dir<'_> = include_dir!("$CARGO_MANIFEST_DIR/../frontend/dist");
 
-pub async fn default_handler(req: Request) -> Response {
-    // check if it is a file, if so return the file
-    // if it is a directory, return the index.html file
-    // otherwise _always_ fallback index.html
-
+/// A simple handler that serves the frontend Studio from STUDIO_DIST.
+pub async fn default_handler(req: Request) -> impl IntoResponse {
     let path = req.uri().path().trim_start_matches('/');
 
-    if let Some(entry) = STUDIO_DIST.get_entry(path) {
-        match entry {
-            DirEntry::Dir(dir_entry) => {
-                let index_html = dir_entry
-                    .get_file("index.html")
-                    .expect("index.html should exist in the dist directory")
-                    .contents_utf8()
-                    .unwrap();
+    // Retrieve the File that according to the path. If it is a directory, see
+    // if there is an index.html file. Always fallback to the index.html in the
+    // root.
+    let file: Option<&File> = STUDIO_DIST
+        .get_entry(path)
+        .and_then(|entry| match entry {
+            DirEntry::Dir(dir_entry) => dir_entry.get_file("index.html"),
+            DirEntry::File(file_entry) => Some(file_entry),
+        })
+        .or_else(|| STUDIO_DIST.get_file("index.html"));
 
-                return (
-                    StatusCode::OK,
-                    [(http::header::CONTENT_TYPE, "text/html")],
-                    index_html,
-                )
-                    .into_response();
-            }
-            DirEntry::File(file_entry) => {
-                let content = file_entry.contents_utf8().unwrap();
-                let content_type =
-                    // some naive content type detection
-                    match file_entry.path().extension().and_then(|ext| ext.to_str()) {
-                        Some("html") => "text/html",
-                        Some("css") => "text/css",
-                        Some("js") => "text/javascript",
-                        Some("ico") => "image/x-icon",
-                        _ => "text/plain",
-                    };
+    // Just return 404 if not file was found.
+    let Some(file) = file else {
+        return StatusCode::NOT_FOUND.into_response();
+    };
 
-                return (
-                    StatusCode::OK,
-                    [(http::header::CONTENT_TYPE, content_type)],
-                    content,
-                )
-                    .into_response();
-            }
-        };
+    let content = file.contents_utf8().unwrap();
+    // Naive content type detection
+    let content_type = match file.path().extension().and_then(|ext| ext.to_str()) {
+        Some("html") => "text/html",
+        Some("css") => "text/css",
+        Some("js") => "text/javascript",
+        Some("ico") => "image/x-icon",
+        _ => "text/plain",
     };
 
     (
         StatusCode::OK,
-        [(http::header::CONTENT_TYPE, "text/html")],
-        STUDIO_DIST
-            .get_file("index.html")
-            .expect("index.html should exist in the dist directory")
-            .contents_utf8()
-            .unwrap(),
+        [(http::header::CONTENT_TYPE, content_type)],
+        content,
     )
         .into_response()
 }
