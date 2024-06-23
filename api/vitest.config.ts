55da8dacf9f6d537067cc4bf583b5ff1bff3781a
Brett Beutell <brbeut@gmail.com>
2024-06-23 08:12:16 -0700

    Add comment about test config

--- api/vitest.config.ts
+++ api/vitest.config.ts
@@ -1,3 +1,6 @@
+// Used example Hono app to configure tests
+// https://github.com/honojs/examples/blob/main/basic/package.json
+
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
