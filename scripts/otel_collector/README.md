c150179e39ec73ba2042817053daff30e3232c12
Benno van den Berg <bennovandenberg@gmail.com>
2024-06-25 14:30:20 -0700

    Add script to start a simple otel collector

---
+++ scripts/otel_collector/README.md
@@ -0,0 +1,5 @@
+# OTEL collector
+
+This directory contains a few scripts and configs to start a OTEL collector.
+
+This requires Docker to be installed and setup.
---
+++ scripts/otel_collector/start.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -euo pipefail
+IFS=$'\n\t'
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+docker run \
+    -p 4317:4317 \
+    -p 4318:4318 \
+    -v "$SCRIPT_DIR/config.yaml:/etc/otelcol-contrib/config.yaml" \
+    otel/opentelemetry-collector-contrib:0.103.1
