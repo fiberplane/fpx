5e5dda7d489c16ddea3436c88a6353ed1c0158ee
Brett Beutell <brbeut@gmail.com>
2024-06-30 11:14:42 -0700

    Fix lint warning about fast refreshes by putting session history context into a folder and separating out hook

--- frontend/src/pages/RequestorPage/RequestorSessionHistoryContext/RequestorSessionHistoryContext.tsx
+++ frontend/src/pages/RequestorPage/RequestorSessionHistoryContext/RequestorSessionHistoryContext.tsx
@@ -19,13 +19,13 @@ type SessionHistoryContextType = {
   recordRequestInSessionHistory: (traceId: RequestorTraceId) => void;
 };
 
-const SessionHistoryContext = createContext<
+export const SessionHistoryContext = createContext<
   SessionHistoryContextType | undefined
 >(undefined);
 
-const RequestorSessionHistoryProvider: React.FC<{ children: ReactNode }> = ({
-  children,
-}) => {
+export const RequestorSessionHistoryProvider: React.FC<{
+  children: ReactNode;
+}> = ({ children }) => {
   const [sessionHistory, setSessionHistoryTraceIds] = useState<
     RequestorTraceId[]
   >([]);
@@ -45,15 +45,3 @@ const RequestorSessionHistoryProvider: React.FC<{ children: ReactNode }> = ({
     </SessionHistoryContext.Provider>
   );
 };
-
-const useSessionHistory = () => {
-  const context = useContext(SessionHistoryContext);
-  if (!context) {
-    throw new Error(
-      "useSessionHistory must be used within a SessionHistoryProvider",
-    );
-  }
-  return context;
-};
-
-export { RequestorSessionHistoryProvider, useSessionHistory };
