8140e240482a90591fac5b69a5691a427f0e22e2
Laurynas Keturakis <laurynas.keturakis@gmail.com>
2024-07-10 14:13:49 +0200

    fix path

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
       include: "**/*.svg",
     }),
     faviconsPlugin({
-      icons: { favicons: { source: "./assets/fpx.svg" } },
+      icons: { favicons: { source: "src/assets/fpx.svg" } },
     }),
   ],
   resolve: {
