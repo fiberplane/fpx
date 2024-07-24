3c9cd9a78e16e9b7873d00116c18d75e5a7fb9c9
Benno van den Berg <bennovandenberg@gmail.com>
2024-07-24 14:37:54 +0200

    Fix index

--- fpx/src/data/migrations/20240708_create_spans.sql
+++ fpx/src/data/migrations/20240708_create_spans.sql
@@ -13,4 +13,4 @@ CREATE TABLE spans (
 ) STRICT;
 
 CREATE INDEX spans_trace_id_span_id ON spans (trace_id, span_id);
-CREATE INDEX spans_trace_id_parent_span_id ON spans (trace_id, span_id);
+CREATE INDEX spans_trace_id_parent_span_id ON spans (trace_id, parent_span_id);
