cfe16df2a38c05abb86888490b5af31133ef8394
Jacco Flenter <flenter@users.noreply.github.com>
2024-07-25 14:35:24 +0800

    Client-library-otel (#83)
    * Initial tracing support
    
    * Very early work still
    
    * Day 2: waitUntil with tracing
    
    * Switch to single instrument function
    
    * Cleanup
    
    * Add back context
    
    * add updated otlp otlp dependency
    
    * refactor
    
    * wip
    
    * Fix linting issues
    
    * Nested spans now works again
    
    * Remove unused dependencies
    
    * Add extra request information
    
    * With measure
    
    * Wait till waitUntil tasks complete
    
    Before calling provider.forceFlush()
    
    * Remove some console.logs
    
    * Use proper export
    
    * Patch all loglevels
    
    * Patch fetch
    
    * Remove unused code
    
    * Cleanup and minor code shuffle
    
    * Remove combined context file
    
    And use the files directly from the `@opentelemetry/context-async-hooks`
    repo
    
    * Tiny changes to the example
    
    * Tiny cleanup
    
    However i noticed that the async-hooks code doens't lint correctly
    
    * Fix import
    
    * Fix linting issue
    
    * Format code
    
    * Make monitoring logs/fetch optional
    
    * Do configurations more in styl of the other lib
    
    * Add attributes to main route span
    
    * Bump nr of loop cycles
    
    * Avoid double patching
    
    * Introduce an isWrapped function
    
    * Remove commented out code
    
    * Further cleanup
    
    * Make measure more flexible
    
    * measure: allow for specifying spanKind
    
    As well as attributes for a span
    
    * Add .fpxconfig to gitingore
    
    * measure: extract out the handling of promise
    
    * Add support for onEnd, onStart and onError
    
    * measure: fixed support for onStart, onEnd, onError
    
    * Await flusing the provider
    
    * Fix linting issue
    
    * Pleasea the typescript linting
    
    * rename package/directory
    
    * Update markdown files
    
    * Update workspaces in package.json
    
    * Update package-lock.json
    
    * Fix proxy for hono app
    
    (when accessing other properties of the app
    
    * Rename arguments
    
    * Remove traces routes
    
    * Remove console.log
    
    * Use swc to build in dist folder
    
    * Add individual attributes for header fields
    
    * Fix formatting
    
    * Feedback on PR
    
    * Fix formatting
    
    * Rename hotel to `sample`
    
    * npmignore sample related files
    
    So they don't get published to npm
    
    * Upgrade some dependencies
    
    * Remove debug log statement

---
+++ client-library-otel/src/constants.ts
@@ -0,0 +1,8 @@
+export const FPX_REQUEST_HEADERS_FULL = "fpx.http.request_headers_full";
+export const FPX_REQUEST_PATHNAME = "fpx.http.request_pathname";
+export const FPX_REQUEST_SEARCH = "fpx.http.request_search";
+export const FPX_REQUEST_SCHEME = "fpx.http.request_scheme";
+export const FPX_REQUEST_BODY = "fpx.http.request_body";
+
+export const FPX_RESPONSE_HEADERS_FULL = "fpx.http.response_headers_full";
+export const FPX_RESPONSE_BODY = "fpx.http.response_body";
