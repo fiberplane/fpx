e0023db4e24422a6d3e5996bb0c21fc448c2fb4a
Brett Beutell <brbeut@gmail.com>
2024-07-25 16:44:19 +0200

    Fix SQL queries

--- frontend/src/pages/RequestDetailsPage/v2/FetchSpan.tsx
+++ frontend/src/pages/RequestDetailsPage/v2/FetchSpan.tsx
@@ -204,10 +204,19 @@ function useVendorSpecificSection(span: MizuSpan) {
 
 function NeonSection({ span }: { span: NeonSpan }) {
   const queryValue = useMemo(() => {
-    return format(span.vendorInfo.sql.query, {
-      language: "postgresql",
-      params: span.vendorInfo.sql.params,
-    });
+    try {
+      const paramsFromNeon = span.vendorInfo.sql.params ?? [];
+      // NOTE - sql-formatter expects the index in the array to match the `$nr` syntax from postgres
+      //        this makes the 0th index unused, but it makes the rest of the indices match the `$1`, `$2`, etc.
+      const params = ["", ...paramsFromNeon];
+      return format(span.vendorInfo.sql.query, {
+        language: "postgresql",
+        params,
+      });
+    } catch (e) {
+      // Being very defensive soz
+      return span?.vendorInfo?.sql?.query ?? "";
+    }
   }, [span]);
   return (
     <SubSection>
