d7cf44e024422591efb480b103504110bc4faece
Brett Beutell <brbeut@gmail.com>
2024-07-27 23:40:21 +0200

    Remove old path matching logic

--- frontend/src/pages/RequestorPage/routes/index.ts
+++ frontend/src/pages/RequestorPage/routes/index.ts
@@ -1,4 +1,3 @@
 export { useRoutes } from "./hooks";
-export { shouldDeselectRoute } from "./path-change-heuristic";
 export { findMatchedRoute } from "./match";
 export { AddRouteButton } from "./AddRouteButton";
