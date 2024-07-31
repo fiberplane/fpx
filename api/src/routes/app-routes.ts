d0dd73f135216773ee6fe9b547525deeb4545d1d
Brett Beutell <brbeut@gmail.com>
2024-07-31 13:50:30 +0200

    Fix typing of serialized requestBody

--- api/src/routes/app-routes.ts
+++ api/src/routes/app-routes.ts
@@ -272,28 +272,39 @@ app.all("/v0/proxy-request/*", async (ctx) => {
   // Extract the request body based on content type
   // *The whole point of this is to serialize the request body into the database, for future reference*
   //
-  // biome-ignore lint/suspicious/noExplicitAny: fix later
-  let requestBody: any;
-  const contentType = ctx.req.header("content-type");
-  if (requestMethod === "GET" || requestMethod === "HEAD") {
-    requestBody = undefined;
-  } else if (contentType?.includes("application/json")) {
-    if (ctx.req.raw.body) {
+  let requestBody:
+    | null
+    | string
+    | {
+        [x: string]: string | File | (string | File)[];
+      } = null;
+  if (ctx.req.raw.body) {
+    const contentType = ctx.req.header("content-type");
+    if (requestMethod === "GET" || requestMethod === "HEAD") {
+      logger.warn(
+        "Request method is GET or HEAD, but request body is not null",
+      );
+      requestBody = null;
+    } else if (contentType?.includes("application/json")) {
+      // NOTE - This kind of handles the case where the body is note valid json,
+      //        but the content type is set to application/json
       const textBody = await ctx.req.text();
       requestBody = safeParseJson(textBody);
+    } else if (contentType?.includes("application/x-www-form-urlencoded")) {
+      const formData = await ctx.req.formData();
+      requestBody = {};
+      // @ts-expect-error - MDN says formData does indeed have an entries method :thinking_face:
+      for (const [key, value] of formData.entries()) {
+        requestBody[key] = value;
+      }
+    } else if (contentType?.includes("multipart/form-data")) {
+      // NOTE - `File` will just show up as an empty object in sqllite - could be nice to record metadata?
+      //         like the name of the file
+      const formData = await ctx.req.parseBody({ all: true });
+      requestBody = formData;
     } else {
-      requestBody = undefined;
+      requestBody = await ctx.req.text();
     }
-  } else if (contentType?.includes("application/x-www-form-urlencoded")) {
-    requestBody = await ctx.req.formData();
-  } else if (contentType?.includes("multipart/form-data")) {
-    // TODO - Test me
-    // NOTE - `File` will just show up as an empty object in sqllite - could be nice to record metadata?
-    //         like the name of the file
-    const formData = await ctx.req.parseBody({ all: true });
-    requestBody = formData;
-  } else {
-    requestBody = await ctx.req.text();
   }
 
   // Record request details
