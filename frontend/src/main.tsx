86262fd9db2f4e68ebedc29847565129e12a37cc
Brett Beutell <brbeut@gmail.com>
2024-08-01 19:06:34 +0200

    Remove dead comment

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-// TOOLTIP PROVIDER
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
