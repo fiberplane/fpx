6a263d4ae87ee804403dbef3b55ea20ee1fc694a
Laurynas Keturakis <laurynas.keturakis@gmail.com>
2024-08-07 16:43:02 +0200

    comments

--- frontend/src/hooks/useWebsocketQueryInvalidation.ts
+++ frontend/src/hooks/useWebsocketQueryInvalidation.ts
@@ -1,6 +1,13 @@
 import { useRealtimeService } from "@/hooks/useRealtimeService";
 import { useQueryClient } from "@tanstack/react-query";
 
+/**
+ * BE CAREFUL WITH THIS HOOK
+ *
+ * This hook is used to invalidate queries when a websocket message is received. It should only
+ * be used at the top-most level of the app, and should not be used in any other components to avoid
+ * infinite re-renders.
+ */
 export function useWebsocketQueryInvalidation() {
   const queryClient = useQueryClient();
 
@@ -9,14 +16,14 @@ export function useWebsocketQueryInvalidation() {
   if (wsMessage) {
     switch (wsMessage.event) {
       case "trace_created": {
-        console.log("trace_created");
+        console.debug("trace_created");
         queryClient.invalidateQueries({ queryKey: wsMessage.payload });
         break;
       }
 
       case "connection_open": {
         // TODO: rewriting some webhook/websocket stuff tbd if this is needed
-        console.log("connection_open");
+        console.debug("connection_open");
         // queryClient.invalidateQueries({
         // queryKey: [WEBHONC_ID_KEY, WEBHONC_REQUEST_KEY],
         // });
@@ -25,7 +32,7 @@ export function useWebsocketQueryInvalidation() {
 
       case "request_incoming": {
         // TODO: rewriting some webhook/websocket stuff tbd if this is needed
-        console.log("request_incoming");
+        console.debug("request_incoming");
         // queryClient.invalidateQueries({
         //   queryKey: [WEBHONC_REQUEST_KEY],
         //   refetchType: "all",
