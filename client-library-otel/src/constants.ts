6e11ff945bfa40dd7b0f1a7c95de2014adb52ca1
Brett Beutell <brbeut@gmail.com>
2024-08-07 12:13:31 +0200

    Remove unused constants

--- client-library-otel/src/constants.ts
+++ client-library-otel/src/constants.ts
@@ -7,12 +7,10 @@ export const EXTRA_SEMATTRS_HTTP_RESPONSE_STATUS_CODE =
   "http.response.status_code";
 export const EXTRA_SEMATTRS_URL_FULL = "url.full";
 
-export const FPX_REQUEST_HEADERS_FULL = "fpx.http.request_headers_full";
 export const FPX_REQUEST_PATHNAME = "fpx.http.request.pathname";
 export const FPX_REQUEST_SEARCH = "fpx.http.request.search";
 export const FPX_REQUEST_SCHEME = "fpx.http.request.scheme";
 export const FPX_REQUEST_BODY = "fpx.http.request.body";
 export const FPX_REQUEST_ENV = "fpx.http.request.env";
 
-export const FPX_RESPONSE_HEADERS_FULL = "fpx.http.response_headers_full";
 export const FPX_RESPONSE_BODY = "fpx.http.response_body";
