32574eb3cb8e369eedd848d58aa446be5c3a0d3c
Brett Beutell <brbeut@gmail.com>
2024-08-08 12:08:20 +0200

    Stylize file upload component better

--- frontend/src/pages/RequestorPage/RequestPanel/FileUploadForm.tsx
+++ frontend/src/pages/RequestorPage/RequestPanel/FileUploadForm.tsx
@@ -1,6 +1,6 @@
 import { Button } from "@/components/ui/button";
 import { cn } from "@/utils";
-import { FilePlusIcon } from "@radix-ui/react-icons";
+import { FileIcon, FilePlusIcon } from "@radix-ui/react-icons";
 import { useCallback } from "react";
 
 type FileUploadFormProps = {
@@ -32,7 +32,7 @@ export const FileUploadForm: React.FC<FileUploadFormProps> = ({
 
   return (
     <div
-      className="flex flex-col items-center justify-center h-full border border-dashed border-gray-500 rounded-md p-4"
+      className="flex flex-col items-center justify-center h-full max-h-[300px] border border-dashed border-gray-500/50 rounded-lg p-4"
       onDrop={handleDrop}
       onDragOver={handleDragOver}
     >
@@ -45,8 +45,12 @@ export const FileUploadForm: React.FC<FileUploadFormProps> = ({
           <label
             htmlFor="file-upload"
             className={cn(
-              "mt-2 cursor-pointer bg-slate-500 text-white px-3 py-1 rounded text-sm",
-              "hover:bg-slate-600",
+              "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50",
+              "px-4 py-2 rounded text-sm",
+              "border border-input bg-background/60 shadow-sm hover:bg-accent hover:text-accent-foreground",
+              "mt-2 cursor-pointer",
+              "text-gray-100",
+              "border-none",
             )}
           >
             Choose file
@@ -60,7 +64,10 @@ export const FileUploadForm: React.FC<FileUploadFormProps> = ({
         </div>
       ) : (
         <div className="flex flex-col items-center justify-center text-center gap-2">
-          <p className="mt-2 text-sm text-gray-400">{file.name}</p>
+          <div className="flex flex-col justify-center items-center gap-2 text-sm text-gray-400">
+            <FileIcon className="w-4 h-4" />
+            {file.name}
+          </div>
           <Button
             className="mt-2 cursor-pointer text-white rounded text-sm px-2 py-1"
             variant="destructive"
