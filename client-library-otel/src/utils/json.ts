43dfb4f810e8d7531c6f12dd3f4b08ada281319f
Brett Beutell <brbeut@gmail.com>
2024-08-09 13:33:31 +0200

    Resolve lint error

--- client-library-otel/src/utils/json.ts
+++ client-library-otel/src/utils/json.ts
@@ -1,6 +1,6 @@
 export function safelySerializeJSON(obj: unknown): string {
   const seen = new WeakSet();
-  return JSON.stringify(obj, (key, value) => {
+  return JSON.stringify(obj, (_key, value) => {
     if (typeof value === "object" && value !== null) {
       if (seen.has(value)) {
         return "[Circular]";
