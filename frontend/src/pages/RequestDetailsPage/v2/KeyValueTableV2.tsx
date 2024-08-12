c977c60249e76a0bbe08d75b33d8988a7bcbcc34
Brett Beutell <brbeut@gmail.com>
2024-08-12 13:49:46 +0200

    Implement the silliest version of collapsible header table

--- frontend/src/pages/RequestDetailsPage/v2/KeyValueTableV2.tsx
+++ frontend/src/pages/RequestDetailsPage/v2/KeyValueTableV2.tsx
@@ -1,25 +1,44 @@
 import { Table, TableBody, TableCell, TableRow } from "@/components/ui/table";
 import { cn } from "@/utils";
-import { useMemo } from "react";
+import { useCallback, useMemo, useState } from "react";
+import { Collapsible, CollapsibleTrigger, CollapsibleContent } from "@/components/ui/collapsible";
+import { Button } from "@/components/ui/button";
 
 export function KeyValueTableV2({
   keyValue,
   emptyMessage = "No data",
   className,
+  defaultMaxRows = 10,
+  defaultCollapsed = true,
 }: {
   keyValue: Record<string, string>;
   emptyMessage?: string;
   className?: string;
+  defaultMaxRows?: number;
+  defaultCollapsed?: boolean;
 }) {
+  const [isCollapsed, setIsCollapsed] = useState(defaultCollapsed);
+  const toggleCollapsed = useCallback(() => {
+    setIsCollapsed(c => !c)
+  }, []);
   const tableEntries = useMemo(() => {
     return Object.entries(keyValue);
   }, [keyValue]);
+
+  const displayedEntries = useMemo(() => {
+    return isCollapsed ? tableEntries.slice(0, defaultMaxRows) : tableEntries;
+  }, [isCollapsed, tableEntries, defaultMaxRows]);
+
+  const hiddenHeadersCount = useMemo(() => {
+    return tableEntries.length - displayedEntries.length;
+  }, [tableEntries, displayedEntries]);
+
   return (
     <div className={cn(className)}>
       <Table className="border-0">
         <TableBody className="">
-          {tableEntries.length > 0 ? (
-            tableEntries.map(([key, value]) => (
+          {displayedEntries.length > 0 ? (
+            displayedEntries.map(([key, value]) => (
               <TableRow key={key}>
                 <TableCell className="px-0 font-medium min-w-[140px] w-[140px] lg:min-w-[200px] uppercase text-xs text-muted-foreground">
                   {key}
@@ -34,8 +53,19 @@ export function KeyValueTableV2({
               </TableCell>
             </TableRow>
           )}
+          {tableEntries.length > defaultMaxRows && (
+            <Collapsible open={!isCollapsed} onOpenChange={toggleCollapsed}>
+              <CollapsibleTrigger asChild>
+                <Button variant="ghost" className="mt-2 text-blue-500">
+                  
+                  {isCollapsed ? `${hiddenHeadersCount} hidden headers - Click to see more` : "Collapse"}
+                </Button>
+              </CollapsibleTrigger>
+            </Collapsible>
+          )}
         </TableBody>
       </Table>
+
     </div>
   );
 }
