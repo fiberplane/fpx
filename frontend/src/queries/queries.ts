ef3ce4c6bb496860ef51bb54ce39e68f2b6f8fce
Laurynas Keturakis <laurynas.keturakis@gmail.com>
2024-08-12 15:10:01 +0200

    fix formatting

--- frontend/src/queries/queries.ts
+++ frontend/src/queries/queries.ts
@@ -1,8 +1,4 @@
-import {
-  QueryClient,
-  QueryClientProvider,
-} from "@tanstack/react-query";
-
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 export const queryClient = new QueryClient();
 export { QueryClientProvider };
