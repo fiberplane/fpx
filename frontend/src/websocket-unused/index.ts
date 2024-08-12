ff90f7280f8f0e5b6e9a9088cd137d0006295e3b
Laurynas Keturakis <laurynas.keturakis@gmail.com>
2024-08-12 15:08:30 +0200

    remove dead code related to old websockets

--- frontend/src/websocket-unused/index.ts
+++
@@ -1,8 +0,0 @@
-/**
- * UNUSED
- *
- * The makings of a module to share a websocket connection across the app.
- */
-
-export { WebSocketProvider } from "./WebSocketContext";
-export { useFpxWebSocket } from "./hooks";
