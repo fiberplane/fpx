b1aca08cdb0392810c8cea504f76b835b8ac58e0
Stephan Lagerwaard <stephanlagerwaard@gmail.com>
2024-08-13 12:00:53 +0200

    Use workspace package for shared types

--- api/src/lib/types.ts
+++ api/src/lib/types.ts
@@ -2,7 +2,6 @@
 
 import type { LibSQLDatabase } from "drizzle-orm/libsql";
 import type { WebSocket } from "ws";
-import { z } from "zod";
 import type * as schema from "../db/schema.js";
 
 export type Bindings = {
@@ -15,35 +14,3 @@ export type Variables = {
   db: LibSQLDatabase<typeof schema>;
   wsConnections: Set<WebSocket>;
 };
-
-/**
- * ==========================================================
- * TODO: move anything below here to a separate package when pnpm monorepo is ready
- * ==========================================================
- */
-
-export type WsMessage = z.infer<typeof WsMessageSchema>;
-
-export const WsMessageSchema = z.discriminatedUnion("event", [
-  z.object({
-    event: z.literal("trace_created"),
-    // TODO: this should be an array of traces instead of the queryKeys to invalidate on the browser
-    payload: z.array(z.literal("mizuTraces")),
-  }),
-  z.object({
-    event: z.literal("connection_open"),
-    payload: z.object({
-      connectionId: z.string(),
-    }),
-  }),
-  z.object({
-    event: z.literal("request_incoming"),
-    payload: z.object({
-      headers: z.record(z.string()),
-      query: z.record(z.string()),
-      path: z.array(z.string()),
-      body: z.any(),
-      method: z.string(),
-    }),
-  }),
-]);
