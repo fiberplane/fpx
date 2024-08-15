5f48ea014507bd879467e00e4b9f873f5efe5e93
あで <adelaide@fiberplane.com>
2024-08-15 18:43:26 +0200

    Use string attributes in otel middleware

--- frontend/src/pages/RequestDetailsPage/v2/otel-helpers.ts
+++ frontend/src/pages/RequestDetailsPage/v2/otel-helpers.ts
@@ -90,6 +90,10 @@ export function getNumber<T = number>(
     defaultValue: T;
   },
 ) {
+  if (typeof value === "string") {
+    return Number(value);
+  }
+
   if (typeof value === "number") {
     return value;
   }
