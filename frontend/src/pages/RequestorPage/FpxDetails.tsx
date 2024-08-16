2f1fd684fb98a15c4fee62eedfab93ec1b506f02
Brett Beutell <brbeut@gmail.com>
2024-08-16 09:52:53 +0200

    Remove ai summaries

--- frontend/src/pages/RequestorPage/FpxDetails.tsx
+++ frontend/src/pages/RequestorPage/FpxDetails.tsx
@@ -4,24 +4,18 @@ import {
   CollapsibleContent,
   CollapsibleTrigger,
 } from "@/components/ui/collapsible";
-import { Skeleton } from "@/components/ui/skeleton";
-import { useAiEnabled } from "@/hooks/useAiEnabled";
+// import { Skeleton } from "@/components/ui/skeleton";
 import { useOtelTrace } from "@/queries";
 import { cn } from "@/utils";
 import { CaretSortIcon, LinkBreak2Icon } from "@radix-ui/react-icons";
-import { useEffect, useState } from "react";
-import ReactMarkdown from "react-markdown";
+import { useState } from "react";
 import {
   getRequestEnv,
   getRequestHeaders,
 } from "../RequestDetailsPage/v2/otel-helpers";
 import { EventsTable } from "./EventsTable";
 import { HeaderTable } from "./HeaderTable";
-import { useSummarizeError } from "./ai";
 import { Requestornator } from "./queries";
-
-// NOTE - Useful for testing rendering of ai markdown in the DOM without hitting openai
-// const MOCK_AI_SUMMARY = getMockAiSummary();
 
 type FpxDetailsProps = {
   response?: Requestornator;
@@ -43,40 +37,9 @@ type TraceDetailsProps = {
 };
 
 function TraceDetails({ response, className }: TraceDetailsProps) {
-  const aiEnabled = useAiEnabled();
-  // HACK - Hide summaries for now because they're obtuse
-  const isAiSummaryEnabled = aiEnabled && false;
-
   const traceId = response.app_responses.traceId;
   const { data: trace, error, isLoading } = useOtelTrace(traceId);
   const isNotFound = !trace && !error && !isLoading;
-
-  const {
-    data: aiSummary,
-    isLoading: isLoadingAiSummary,
-    isFetching: isFetchingAiSummary,
-    isRefetching: isRefetchingAiSummary,
-    refetch: fetchAiSummary,
-  } = useSummarizeError(trace);
-
-  // TODO - use query key with trace id in fetch call so we can avoid unnecessary requests when other values update...
-  const lastFetchedTraceId = aiSummary?.traceId;
-  useEffect(() => {
-    if (
-      trace &&
-      trace?.[0]?.trace_id !== lastFetchedTraceId &&
-      isAiSummaryEnabled &&
-      !isFetchingAiSummary
-    ) {
-      fetchAiSummary();
-    }
-  }, [
-    trace,
-    lastFetchedTraceId,
-    fetchAiSummary,
-    isAiSummaryEnabled,
-    isFetchingAiSummary,
-  ]);
 
   if (isNotFound) {
     return <div>Trace not found</div>;
@@ -93,28 +56,6 @@ function TraceDetails({ response, className }: TraceDetailsProps) {
 
   return (
     <div className={cn("mt-2", className)}>
-      {isAiSummaryEnabled && (
-        <div className="">
-          <h3 className="pt-1 pb-2 text-sm">Summary</h3>
-          <div className="font-light">
-            {isLoadingAiSummary ||
-            isFetchingAiSummary ||
-            isRefetchingAiSummary ? (
-              <div className="p2">
-                <div className="text-sm text-gray-400 mb-2">
-                  Loading AI Summary...
-                </div>
-                <Skeleton className="h-24 w-full rounded-md mb-2" />
-              </div>
-            ) : aiSummary && aiSummary?.summary ? (
-              <AiSummary summary={aiSummary.summary} />
-            ) : (
-              <div className="p2">Nothing to show, soz</div>
-            )}
-          </div>
-        </div>
-      )}
-
       <Section title="Events" defaultIsOpen>
         <EventsTable events={events} />
       </Section>
@@ -202,74 +143,6 @@ function Section({
 
 function SectionTitle({ children }: { children: React.ReactNode }) {
   return <div className="rounded-md mt-2 pt-1 pb-2 text-sm">{children}</div>;
-}
-
-function AiSummary({ summary }: { summary: string }) {
-  return (
-    <div className="text-sm font-light border p-2 rounded max-w-[680px]">
-      <ReactMarkdown
-        components={{
-          // eslint-disable-next-line @typescript-eslint/no-unused-vars
-          h1: ({ node, ...props }) => (
-            <h1 className="text-lg font-bold my-2 text-gray-400" {...props} />
-          ),
-          // eslint-disable-next-line @typescript-eslint/no-unused-vars
-          h2: ({ node, ...props }) => (
-            <h2
-              className="text-lg font-semibold my-2 text-gray-400"
-              {...props}
-            />
-          ),
-          // eslint-disable-next-line @typescript-eslint/no-unused-vars
-          h3: ({ node, ...props }) => (
-            <h3
-              className="text-md font-semibold mt-2 mb-1 text-gray-300"
-              {...props}
-            />
-          ),
-          // eslint-disable-next-line @typescript-eslint/no-unused-vars
-          strong: ({ node, ...props }) => (
-            <strong className="font-semibold" {...props} />
-          ),
-          code: ({ node, children, className, ...props }) => {
-            const match = /language-(\w+)/.exec(className || "");
-            const language = match ? match[1] : "";
-            let color = "text-red-300";
-            const otherClasses: Array<string> = [];
-
-            if (language) {
-              color = "text-gray-300";
-              otherClasses.push("my-3 border p-1 block rounded");
-            }
-
-            if (language === "json") {
-              console.log("JSON", node, children);
-              // TODO
-            }
-            if (language === "sql") {
-              console.log("SQL", node, children);
-              // TODO
-            }
-            if (language === "javascript") {
-              console.log("javascript", node, children);
-              // TODO
-            }
-            return (
-              <code
-                className={cn(className, color, ...otherClasses)}
-                {...props}
-              >
-                {children}
-              </code>
-            );
-          },
-          // Add more custom components as needed
-        }}
-      >
-        {summary}
-      </ReactMarkdown>
-    </div>
-  );
 }
 
 function NoTrace() {
