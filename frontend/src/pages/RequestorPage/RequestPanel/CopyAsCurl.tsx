d0e58b5d4ff658824f80627a8b1230e6c72a607d
oscarvz <oscar@fiberplane.com>
2024-08-20 11:27:52 +0200

    Fix body payload

--- frontend/src/pages/RequestorPage/RequestPanel/CopyAsCurl.tsx
+++ frontend/src/pages/RequestorPage/RequestPanel/CopyAsCurl.tsx
@@ -6,6 +6,10 @@ type CopyAsCurlProps = Pick<
   "body" | "method" | "path" | "requestHeaders" | "queryParams"
 >;
 
+/**
+ * Copy the current request as a curl command to the clipboard. It currently
+ * only supports JSON bodies.
+ */
 export function CopyAsCurl({
   body,
   method,
@@ -14,8 +18,7 @@ export function CopyAsCurl({
   requestHeaders,
 }: CopyAsCurlProps) {
   const handleClick = async () => {
-    // TODO: Validate JSON payload first this is also passing the type
-    const parsedBody = JSON.stringify(body);
+    const payload = jsonBody({ body, method });
 
     const headers = requestHeaders.reduce(
       (acc, { enabled, key, value }) =>
@@ -24,13 +27,15 @@ export function CopyAsCurl({
     );
 
     const url = new URL(path);
-    for (const queryParam of queryParams) {
-      if (queryParam.enabled) {
-        url.searchParams.append(queryParam.key, queryParam.value);
+    for (const { enabled, key, value } of queryParams) {
+      if (enabled) {
+        url.searchParams.append(key, value);
       }
     }
 
-    const curlCommand = `curl -X ${method} ${url} ${headers} -d '${parsedBody}'`;
+    const curlCommand = `curl -X ${method} ${url} ${headers} ${
+      payload ? `-d '${payload}'` : ""
+    }`;
 
     try {
       await navigator.clipboard.writeText(curlCommand);
@@ -44,4 +49,16 @@ export function CopyAsCurl({
       copy as curl
     </Button>
   );
+}
+
+/**
+ * Prevent sending JSON body for GET and HEAD requests. If the method is valid
+ * and the body is JSON, return the JSON stringified body.
+ */
+function jsonBody({ body, method }: Pick<RequestorState, "body" | "method">) {
+  if (method === "GET" || method === "HEAD" || body.type !== "json") {
+    return undefined;
+  }
+
+  return JSON.stringify(body.value);
 }
