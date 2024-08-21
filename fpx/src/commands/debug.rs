f38d7c622c3b309da0b81699e3fc09dd8d2d400a
Benno van den Berg <bennovandenberg@gmail.com>
2024-08-21 17:20:51 +0200

    Add debug command to print the location of the fpx-directory

--- fpx/src/commands/debug.rs
+++ fpx/src/commands/debug.rs
@@ -1,3 +1,4 @@
+use crate::find_fpx_dir;
 use anyhow::Result;
 use clap::Subcommand;
 
@@ -11,6 +12,9 @@ pub struct Args {
 
 #[derive(Subcommand, Debug)]
 pub enum Command {
+    /// Print the path to the FPX directory.
+    FpxDirectory,
+
     /// Start a WebSocket connection to the server. This will dump any message
     /// it receives to the console.
     #[clap(alias = "ws")]
@@ -18,6 +22,16 @@ pub enum Command {
 }
 pub async fn handle_command(args: Args) -> Result<()> {
     match args.command {
+        Command::FpxDirectory => handle_fpx_directory_command().await,
         Command::WebSocket(args) => ws::handle_command(args).await,
     }
+}
+
+pub async fn handle_fpx_directory_command() -> Result<()> {
+    let fpx_directory = find_fpx_dir()?;
+    match fpx_directory {
+        Some(path) => eprintln!("Fpx directory found: {}", path.display()),
+        None => eprintln!("Fpx directory not found"),
+    }
+    Ok(())
 }
