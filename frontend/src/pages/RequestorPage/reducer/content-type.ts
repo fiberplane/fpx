499d7bc9f7086621c3560a6e2b474b727f857cf8
Brett Beutell <brbeut@gmail.com>
2024-08-21 11:10:23 +0200

    Iron out behavior of automatic content-type header helper

--- frontend/src/pages/RequestorPage/reducer/content-type.ts
+++ frontend/src/pages/RequestorPage/reducer/content-type.ts
@@ -5,6 +5,21 @@ import {
 import { isDraftParameter } from "../KeyValueForm/data";
 import { RequestorBody, RequestorState } from "./state";
 
+/**
+ * This makes sure to synchronize the content type header with the body type.
+ * But there are a few caveats!
+ *
+ * - If the body is a multipart form, we don't want to add the content type header
+ *   We only want to remove the content type header if it's set to text/* or application/json
+ *
+ * - If the body is a file, we only want to remove the content type header if it's set to text/* or application/json
+ *   We don't want to add a content type header for files, to give the user the ability to set the content type themselves,
+ *   otherwise fetch will just add application/octet-stream
+ *
+ * - If the body is a json, we want to set/update the content type to application/json
+ *
+ * - If the body is a text, we want to set/update the content type to text/plain
+ */
 export function addContentTypeHeader(state: RequestorState): RequestorState {
   const currentHeaders = state.requestHeaders;
   const currentContentTypeHeader = getCurrentContentType(state);
@@ -92,14 +107,22 @@ function getUpdateOperation(
 ) {
   const nextContentTypeValue = mapBodyToContentType(currentBody);
 
+  // `null` means "no change"
   if (currentContentTypeHeader?.value?.startsWith(nextContentTypeValue)) {
     return null;
   }
 
+  // If there's no content type header, we might want to add one
   if (!currentContentTypeHeader) {
-    // HACK - Avoid adding the content type header when it's already set to multipart/form-data
-    //        We don't want to mess up the form boundary added by fetch
+    // HACK - Avoid adding the content type header when the body is multipart form-data
+    //        We don't want to mess up the form boundary added by fetch (it will cause errors)
     if (currentBody.type === "form-data" && currentBody.isMultipart) {
+      return null;
+    }
+
+    // If the body is a file, we don't want to add the content type header, in order to give the user the ability to set the content type themselves
+    // If the user doesn't define a content type, fetch will just add application/octet-stream
+    if (currentBody.type === "file") {
       return null;
     }
 
@@ -115,11 +138,26 @@ function getUpdateOperation(
     };
   }
 
-  if (currentBody.type === "form-data" && currentBody.isMultipart) {
+  // Remove the content type header if the new body is multipart form-data,
+  // and the user hasn't specified a multipart/form-data content type
+  if (
+    currentBody.type === "form-data" &&
+    currentBody.isMultipart &&
+    !currentContentTypeHeader.value?.startsWith("multipart/form-data")
+  ) {
     return {
       type: "remove",
       value: currentContentTypeHeader,
     };
+  }
+
+  if (currentBody.type === "file") {
+    if (currentContentTypeHeader.value?.startsWith("text/") || currentContentTypeHeader.value?.startsWith("application/json")) {
+      return {
+        type: "remove",
+        value: currentContentTypeHeader,
+      };
+    }
   }
 
   // Update the content type header
