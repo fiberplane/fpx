3e9d9f472fd5754692bcc7537d2df5bbcbaef8ed
Brett Beutell <brbeut@gmail.com>
2024-08-22 14:15:41 +0200

    Ship it all i will fix it on my end

--- www/astro.config.mjs
+++ www/astro.config.mjs
@@ -16,38 +16,7 @@ export default defineConfig({
         github: "https://github.com/fiberplane/fpx",
         discord: "https://discord.com/invite/cqdY6SpfVR",
       },
-      sidebar: [
-        {
-          label: "Home",
-          items: [
-            // Each item here is one entry in the navigation menu.
-            { label: "Get started", slug: "home/get-started" },
-          ],
-        },
-        {
-          label: "Components",
-          items: [
-            { label: "Client library", slug: "components/client-library" },
-            { label: "Studio", slug: "components/studio" },
-          ],
-        },
-        {
-          label: "Features",
-          items: [
-            { label: "Making requests", slug: "features/making-requests" },
-            { label: "Showing traces", slug: "features/showing-traces" },
-            {
-              label: "Generating with AI",
-              slug: "features/generating-with-ai",
-            },
-            {
-              label: "Generating prompts for tests",
-              slug: "features/generating-prompts-for-tests",
-            },
-            { label: "Webhooks", slug: "features/webhooks" },
-          ],
-        },
-      ],
+
       components: {
         Header: "@/components/Header.astro",
         Pagination: "@/components/Pagination.astro",
