b9df0517ca48ad8cbc11ac87dac0281b30a4879d
Laurynas Keturakis <laurynas.keturakis@gmail.com>
2024-08-26 22:04:04 +0200

    scaffold blog setup

--- www/src/content/config.ts
+++ www/src/content/config.ts
@@ -1,6 +1,22 @@
-import { defineCollection } from "astro:content";
+import { defineCollection, z } from "astro:content";
 import { docsSchema } from "@astrojs/starlight/schema";
 
+const blogSchema = z.object({
+	title: z.string(),
+	description: z.string(),
+	date: z.coerce.date(),
+	author: z.string(),
+	tags: z.array(z.string()),
+	image: z.object({
+		src: z.string(),
+		alt: z.string(),
+	}).optional(),
+});
+
 export const collections = {
-  docs: defineCollection({ schema: docsSchema() }),
+	docs: defineCollection({ schema: docsSchema() }),
+	blog: defineCollection({
+		type: "content",
+		schema: blogSchema
+	}),
 };
