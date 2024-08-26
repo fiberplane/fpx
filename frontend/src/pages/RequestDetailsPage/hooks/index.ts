21fd335d42f18cbfd7388ccc3d90a237b4fb2f6c
Laurynas Keturakis <laurynas.keturakis@gmail.com>
2024-08-26 19:50:49 +0200

    add useMostRecentRequest hook

--- frontend/src/pages/RequestDetailsPage/hooks/index.ts
+++ frontend/src/pages/RequestDetailsPage/hooks/index.ts
@@ -1,3 +1,5 @@
 export { useEscapeToList } from "./useNavigateToList";
 export { usePagination } from "./usePagination";
 export { useReplayRequest } from "./useReplayRequest";
+export { useMostRecentRequest } from "./useMostRecentRequest";
+export { useShouldReplay } from "./useShouldReplay";
