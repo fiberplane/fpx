39b1c103cd0adb85d1d7cb20f06813cf165120e1
Benno van den Berg <hatchan@users.noreply.github.com>
2024-08-27 10:01:26 +0200

    Allow spans to be deleted (#185)
    * Allow spans to be deleted
    
    Allow deleting a specific span or all spans of a trace
    
    * Implement delete fn's on D1Store
    
    * Remove irrelevant assignments

--- fpx/src/commands/client/traces.rs
+++ fpx/src/commands/client/traces.rs
@@ -17,12 +17,16 @@ pub enum Command {
 
     /// List all traces
     List(ListArgs),
+
+    /// Delete all spans for a single trace
+    Delete(DeleteArgs),
 }
 
 pub async fn handle_command(args: Args) -> Result<()> {
     match args.command {
         Command::Get(args) => handle_get(args).await,
         Command::List(args) => handle_list(args).await,
+        Command::Delete(args) => handle_delete(args).await,
     }
 }
 
@@ -59,6 +63,24 @@ async fn handle_list(args: ListArgs) -> Result<()> {
     let result = api_client.trace_list().await?;
 
     serde_json::to_writer_pretty(stdout(), &result)?;
+
+    Ok(())
+}
+
+#[derive(clap::Args, Debug)]
+pub struct DeleteArgs {
+    /// TraceID - hex encoded
+    pub trace_id: String,
+
+    /// Base url of the fpx dev server.
+    #[arg(from_global)]
+    pub base_url: Url,
+}
+
+async fn handle_delete(args: DeleteArgs) -> Result<()> {
+    let api_client = ApiClient::new(args.base_url.clone());
+
+    api_client.trace_delete(args.trace_id).await?;
 
     Ok(())
 }
