a319a929c63b21a943f700ddb4c7faed59eaa307
Stephan Lagerwaard <stephanlagerwaard@gmail.com>
2024-08-28 17:41:25 +0200

    Fix migration

--- api/drizzle/0012_worthless_mystique.sql
+++ api/drizzle/0012_worthless_mystique.sql
@@ -3,11 +3,11 @@ CREATE TABLE `otel_spans_new` (
 	`span_id` text NOT NULL,
 	`trace_id` text NOT NULL
 );
-
+--> statement-breakpoint
 INSERT INTO `otel_spans_new` (`inner`, `span_id`, `trace_id`)
-SELECT `inner`, `span_id`, `trace_id`
+SELECT `parsed_payload`, `span_id`, `trace_id`
 FROM `otel_spans`;
-
+--> statement-breakpoint
 DROP TABLE `otel_spans`;
-
+--> statement-breakpoint
 ALTER TABLE `otel_spans_new` RENAME TO `otel_spans`;
