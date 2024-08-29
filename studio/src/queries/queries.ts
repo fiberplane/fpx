c7c16c0aea36bd3fed9b102d4653078f10ae2cf1
Laurynas Keturakis <laurynas.keturakis@gmail.com>
2024-08-29 18:10:16 +0200

    increase basic config for stale and gc Times

--- studio/src/queries/queries.ts
+++ studio/src/queries/queries.ts
@@ -1,6 +1,14 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-export const queryClient = new QueryClient();
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    mutations: {},
+    queries: {
+      staleTime: 20 * 60 * 1000, // 20 minutes in milliseconds
+      gcTime: 24 * 60 * 60 * 1000, // 24 hours in milliseconds
+    },
+  },
+});
 export { QueryClientProvider };
 
 export const MIZU_TRACES_KEY = "mizuTraces";
