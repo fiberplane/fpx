a6797265b1f41a72c4701bc48bdbbca051352221
Laurynas Keturakis <laurynas.keturakis@gmail.com>
2024-08-30 17:54:21 +0200

    formatting

--- www/src/content/config.ts
+++ www/src/content/config.ts
@@ -1,27 +1,29 @@
 import { defineCollection, z } from "astro:content";
 import { docsSchema } from "@astrojs/starlight/schema";
 
-
 const docs = defineCollection({
-	type: "content",
-	schema: docsSchema() });
+  type: "content",
+  schema: docsSchema(),
+});
 
 const blog = defineCollection({
-	type: "content",
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		date: z.coerce.date(),
-		author: z.string(),
-		tags: z.array(z.string()),
-		image: z.object({
-			src: z.string(),
-			alt: z.string(),
-		}).optional(),
-})
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    date: z.coerce.date(),
+    author: z.string(),
+    tags: z.array(z.string()),
+    image: z
+      .object({
+        src: z.string(),
+        alt: z.string(),
+      })
+      .optional(),
+  }),
 });
 
 export const collections = {
-	docs,
-	blog
+  docs,
+  blog,
 };
