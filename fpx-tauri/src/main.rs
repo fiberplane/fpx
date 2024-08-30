521e101bd8428e97be61d2d88dcb794da326d8e3
Stephan Lagerwaard <stephanlagerwaard@gmail.com>
2024-08-30 14:56:09 +0200

    Remember window position

--- fpx-tauri/src/main.rs
+++ fpx-tauri/src/main.rs
@@ -2,7 +2,8 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
-  tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        .plugin(tauri_plugin_window_state::Builder::default().build())
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
