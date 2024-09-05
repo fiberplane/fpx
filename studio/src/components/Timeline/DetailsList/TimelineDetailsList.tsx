3278b72765ea1ca783ce57f183067fb18cf80074
Laurynas Keturakis <laurynas.keturakis@gmail.com>
2024-09-05 16:18:52 +0200

    fix the individual timeline detail comps to take up full vertical space

--- studio/src/components/Timeline/DetailsList/TimelineDetailsList.tsx
+++ studio/src/components/Timeline/DetailsList/TimelineDetailsList.tsx
@@ -30,6 +30,7 @@ function TimelineListDetailsComponent({
             "hover:bg-primary/10 hover:border-blue-500 hover:rounded-l-none",
             "data-[highlighted=true]:bg-primary/10",
             "relative after:absolute after:bottom-[-4px] after:bg-muted-foreground/30 after:w-full after:h-px last:after:h-0",
+            "h-full",
           )}
           data-highlighted={highlightedSpanId === getId(item)}
         >
