7da3f55e4664bf3051dfc65acdbdf5940d604369
Laurynas Keturakis <laurynas.keturakis@gmail.com>
2024-09-13 11:51:07 +0200

    initial move to the bottom

--- studio/src/Layout/Layout.tsx
+++ studio/src/Layout/Layout.tsx
@@ -15,7 +15,7 @@ import { NavLink } from "react-router-dom";
 import FpxIcon from "../assets/fpx.svg";
 import { WebhoncBadge } from "../components/WebhoncBadge";
 import { Button } from "../components/ui/button";
-import { DialogTitle } from "../components/ui/dialog";
+import { DialogPortal, DialogTitle } from "../components/ui/dialog";
 import { useWebsocketQueryInvalidation } from "../hooks";
 import { useProxyRequestsEnabled } from "../hooks/useProxyRequestsEnabled";
 import { SettingsPage } from "../pages/SettingsPage/SettingsPage";
@@ -43,8 +43,14 @@ export const Layout: React.FC<{ children?: React.ReactNode }> = ({
 
   return (
     <div className="flex min-h-screen w-full flex-col bg-muted/30 max-w-128 overflow-hidden">
-      <nav className="flex gap-4 sm:gap-4 py-2 sm:py-2 justify-between items-center border-b">
-        <div className="sticky top-0 flex items-center gap-2 px-4 sm:static sm:h-auto border-0 bg-transparent md:px-6 text-sm">
+      <main
+        className={cn("md:gap-8", "overflow-hidden", "h-[calc(100vh-40px)]")}
+      >
+        {children}
+      </main>
+
+      <nav className="flex gap-4 sm:gap-4 bg-muted/50 pb-2 pt-1 justify-between items-center border-b">
+        <div className="flex items-center gap-2 px-4 sm:static sm:h-auto border-0 bg-transparent md:px-6 text-sm">
           <SidePanelTrigger />
           <FloatingSidePanel />
           <HeaderNavLink to="/">
@@ -61,65 +67,35 @@ export const Layout: React.FC<{ children?: React.ReactNode }> = ({
               <WebhoncBadge />
             </div>
           )}
-          <div className="flex items-center border-l gap-1 px-1">
+          <div className="flex items-center gap-1 px-1">
             <Root>
               <DialogTrigger asChild>
                 <Button variant="ghost" size="icon" className="p-0.5 w-6 h-6">
                   <Icon icon="lucide:settings" />
                 </Button>
               </DialogTrigger>
-              <DialogContent className="fixed top-0 left-0 w-full h-full z-50">
-                <div className="h-full grid bg-background items-start justify-center">
-                  <div className="pr-1 overflow-hidden w-full lg:max-w-[1060px] md:max-w-[1100px] grid grid-rows-[auto_1fr]">
-                    <div className="flex justify-between items-center px-8 pr-8 lg:px-12 lg:pr-10 pt-6">
-                      <DialogTitle>Settings</DialogTitle>
-                      <DialogClose asChild>
-                        <Button
-                          variant="ghost"
-                          size="icon"
-                          className="p-0.5 w-6 h-6"
-                        >
-                          <Icon icon="lucide:x" />
-                        </Button>
-                      </DialogClose>
-                    </div>
-                    <div>
-                      <DialogDescription className="px-8 pr-8 lg:px-12 lg:pr-10 pt-6 text-sm text-muted-foreground">
-                        Manage your settings and preferences.
-                      </DialogDescription>
-
-                      <SettingsPage />
-                    </div>
-                  </div>
-                </div>
-              </DialogContent>
             </Root>
-            <Button variant="ghost" size="icon" className="p-0.5 w-6 h-6">
-              <a
-                href="https://github.com/fiberplane/fpx"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <GitHubLogoIcon className="w-3.5 h-3.5" />
-              </a>
-            </Button>
-            <Button variant="ghost" size="icon" className="p-0.5 w-6 h-6">
-              <a
-                href="https://discord.com/invite/cqdY6SpfVR"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <DiscordLogoIcon className="w-3.5 h-3.5" />
-              </a>
-            </Button>
+            {/* <Button variant="ghost" size="icon" className="p-0.5 w-6 h-6"> */}
+            {/*   <a */}
+            {/*     href="https://github.com/fiberplane/fpx" */}
+            {/*     target="_blank" */}
+            {/*     rel="noopener noreferrer" */}
+            {/*   > */}
+            {/*     <GitHubLogoIcon className="w-3.5 h-3.5" /> */}
+            {/*   </a> */}
+            {/* </Button> */}
+            {/* <Button variant="ghost" size="icon" className="p-0.5 w-6 h-6"> */}
+            {/*   <a */}
+            {/*     href="https://discord.com/invite/cqdY6SpfVR" */}
+            {/*     target="_blank" */}
+            {/*     rel="noopener noreferrer" */}
+            {/*   > */}
+            {/*     <DiscordLogoIcon className="w-3.5 h-3.5" /> */}
+            {/*   </a> */}
+            {/* </Button> */}
           </div>
         </div>
       </nav>
-      <main
-        className={cn("md:gap-8", "overflow-hidden", "h-[calc(100vh-40px)]")}
-      >
-        {children}
-      </main>
     </div>
   );
 };
@@ -156,5 +132,38 @@ const SidePanelTrigger = () => {
     </Button>
   );
 };
+
+function SettingsScreen({
+  settingsOpen,
+  setSettingsOpen,
+}: { settingsOpen: boolean; setSettingsOpen: (open: boolean) => void }) {
+  return (
+    <Root open={settingsOpen} onOpenChange={setSettingsOpen}>
+      <DialogPortal>
+        <DialogContent className="fixed top-0 left-0 w-full h-full z-50">
+          <div className="h-full grid bg-background items-start justify-center">
+            <div className="pr-1 overflow-hidden w-full lg:max-w-[1060px] md:max-w-[1100px] grid grid-rows-[auto_1fr]">
+              <div className="flex justify-between items-center px-8 pr-8 lg:px-12 lg:pr-10 pt-6">
+                <DialogTitle>Settings</DialogTitle>
+                <DialogClose asChild>
+                  <Button variant="ghost" size="icon" className="p-0.5 w-6 h-6">
+                    <Icon icon="lucide:x" />
+                  </Button>
+                </DialogClose>
+              </div>
+              <div>
+                <DialogDescription className="px-8 pr-8 lg:px-12 lg:pr-10 pt-6 text-sm text-muted-foreground">
+                  Manage your settings and preferences.
+                </DialogDescription>
+
+                <SettingsPage />
+              </div>
+            </div>
+          </div>
+        </DialogContent>
+      </DialogPortal>
+    </Root>
+  );
+}
 
 export default Layout;
