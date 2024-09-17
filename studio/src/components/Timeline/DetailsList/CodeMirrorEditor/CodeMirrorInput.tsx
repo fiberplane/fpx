aa5a30bccf3c3d69a97edd76e63ff068b6b78bda
Brett Beutell <brbeut@gmail.com>
2024-09-17 16:46:47 +0200

    Modify readonly styles and fix jumping issue

--- studio/src/components/Timeline/DetailsList/CodeMirrorEditor/CodeMirrorInput.tsx
+++ studio/src/components/Timeline/DetailsList/CodeMirrorEditor/CodeMirrorInput.tsx
@@ -6,12 +6,10 @@ import { useMemo, useState } from "react";
 
 const inputTheme = EditorView.theme({
   "&": {
+    background: "transparent !important",
     // HACK - Using tailwind `text-sm` values
     fontSize: "0.875rem",
     lineHeight: "1.25rem",
-
-    background: "transparent !important",
-    // letterSpacing: "0.025em",
   },
   ".cm-gutters": {
     borderColor: "transparent",
@@ -21,15 +19,6 @@ const inputTheme = EditorView.theme({
     borderLeftColor: "white",
   },
 });
-
-type CodeMirrorEditorProps = {
-  readOnly?: boolean;
-  minHeight?: string;
-  width?: string;
-  value?: string;
-  onChange: (value?: string) => void;
-  placeholder?: string;
-};
 
 // Configuration to disable all basic setup features on code mirror instance, so it behaves more like a text input
 const CODE_MIRROR_BASIC_SETUP_DISABLE_ALL = {
@@ -53,6 +42,11 @@ const CODE_MIRROR_BASIC_SETUP_DISABLE_ALL = {
   lintKeymap: false,
 };
 
+// An extension that does nothing to make conditional extensions easier,
+// e.g., `isFocused ? noopExtension : inputTrucateExtension`
+const noopExtension = EditorView.theme({});
+
+// Apply styles for the editor to make it look like one of our inputs
 const inputBaseStylesExtension = EditorView.theme({
   ".cm-placeholder": {
     color: "hsl(var(--muted-foreground))",
@@ -66,8 +60,8 @@ const inputBaseStylesExtension = EditorView.theme({
   },
 });
 
-// Hacky extension to modify the editor to look like an input and truncate text that overflows
-// (This is used when the editor/input is not in focus)
+// Extension to truncate text that overflows
+// We only want ot use this when the editor is *not* focused
 const inputTrucateExtension = EditorView.theme({
   ".cm-content": {
     whiteSpace: "nowrap",
@@ -82,36 +76,72 @@ const inputTrucateExtension = EditorView.theme({
   },
 });
 
+// Extension for readonly mode, which makes the editor's cursor transparent,
+// but uses a pointer cursor when the user hovers over the input,
+// to indicate that you can click on it to expand it (in case the path parameter is truncated)
+const readonlyExtension = EditorView.theme({
+  ".cm-cursor": {
+    borderLeftColor: "transparent !important",
+  },
+  ".cm-content": {
+    cursor: "pointer !important",
+  },
+});
+
 // Hide the gutter on the code mirror instance
 const hiddenGutterExtension = gutter({ class: "hidden border-none" });
 
+type CodeMirrorEditorProps = {
+  readOnly?: boolean;
+  width?: string;
+  value?: string;
+  onChange: (value?: string) => void;
+  placeholder?: string;
+};
+
 export function CodeMirrorInput(props: CodeMirrorEditorProps) {
-  const {
-    value,
-    onChange,
-    placeholder,
-    width,
-    minHeight = "28px",
-    readOnly,
-  } = props;
+  const { value, onChange, placeholder, width, readOnly } = props;
 
-  // State to manage focus
   const [isFocused, setIsFocused] = useState(false);
-
-  // Dynamic height based on focus state
-  const dynamicHeight = isFocused ? "auto" : minHeight;
 
   const style = useMemo(
     () => ({
-      width: width ?? "100%", // if specified, this is a fixed width
-      height: dynamicHeight, // dynamic height based on focus
-      overflow: isFocused ? "auto" : "hidden", // show overflown context when focused, hide when not
-      whiteSpace: isFocused ? "normal" : "nowrap", // change to ellipsis text truncation on blur
+      // if specified, the width should be a fixed width, otherwise we take up full width
+      width: width ?? "100%",
+      height: "auto",
+      // show overflown content when focused, hide when not
+      overflow: isFocused ? "auto" : "hidden",
+      // change to ellipsis text truncation on blur
+      whiteSpace: isFocused ? "normal" : "nowrap",
       textOverflow: isFocused ? "clip" : "ellipsis",
-      cursor: "text", // show text cursor when hovering
+      // show text cursor when hovering
+      cursor: "text",
     }),
-    [width, dynamicHeight, isFocused],
+    [width, isFocused],
   );
+
+  const extensions = useMemo(() => {
+    return [
+      hiddenGutterExtension,
+      inputBaseStylesExtension,
+      //
+      /**
+       * NOTE: This is the CSS added by lineWrapping:
+       
+        ".cm-lineWrapping": {
+            whiteSpace_fallback: "pre-wrap", // For IE
+            whiteSpace: "break-spaces",
+            wordBreak: "break-word", // For Safari, which doesn't support overflow-wrap: anywhere
+            overflowWrap: "anywhere",
+            flexShrink: 1
+          }
+
+       */
+      EditorView.lineWrapping,
+      readOnly ? readonlyExtension : noopExtension,
+      isFocused ? noopExtension : inputTrucateExtension,
+    ];
+  }, [isFocused, readOnly]);
 
   return (
     <div
@@ -119,8 +149,10 @@ export function CodeMirrorInput(props: CodeMirrorEditorProps) {
         "rounded border border-transparent",
         "focus-visible:outline-none",
         {
-          "border border-blue-600": isFocused,
-          // HACK - This prevents a "jump" for the placeholder or input text when clicking on the input
+          "border border-blue-600": isFocused && !readOnly,
+          "border border-gray-600/50": isFocused && readOnly,
+          // HACK - This prevents a horizontal "jump" for the placeholder or input text when clicking on the input
+          // NOTE - There are still issues with vertical jumping on focus :(
           "border-l border-transparent": !isFocused,
         },
       )}
@@ -129,26 +161,10 @@ export function CodeMirrorInput(props: CodeMirrorEditorProps) {
       <CodeMirror
         value={value}
         onChange={onChange}
-        height={dynamicHeight}
         theme={[inputTheme]}
         indentWithTab={false}
         readOnly={readOnly}
-        extensions={[
-          hiddenGutterExtension,
-          inputBaseStylesExtension,
-          EditorView.lineWrapping,
-          // NOTE: This is the CSS added by lineWrapping:
-          /**
-           ".cm-lineWrapping": {
-              whiteSpace_fallback: "pre-wrap", // For IE
-              whiteSpace: "break-spaces",
-              wordBreak: "break-word", // For Safari, which doesn't support overflow-wrap: anywhere
-              overflowWrap: "anywhere",
-              flexShrink: 1
-            },
-           */
-          isFocused ? EditorView.lineWrapping : inputTrucateExtension,
-        ]}
+        extensions={extensions}
         onFocus={() => setIsFocused(true)} // Set focus to true
         onBlur={() => setIsFocused(false)} // Set focus to false
         basicSetup={CODE_MIRROR_BASIC_SETUP_DISABLE_ALL}
