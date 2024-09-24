939d901ebafddb68bb4dca85fafecdbca0872ec2
Brett Beutell <brbeut@gmail.com>
2024-09-24 19:16:34 +0200

    Add janky test for recursively expanding out of scope vars ((needs work))

--- examples/test-static-analysis/src/utils.ts
+++ examples/test-static-analysis/src/utils.ts
@@ -1,5 +1,10 @@
 import type { HonoRequest } from "hono";
+import { RANDOM_HEADER } from "./constants";
 
 export function getAuthHeader(req: HonoRequest) {
   return req.header("Authorization");
+}
+
+export function getRandomHeader(req: HonoRequest) {
+  return req.header(RANDOM_HEADER);
 }
