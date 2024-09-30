5cf298980f3efb43c6ce94439bd40f7a18e1705b
Brett Beutell <brbeut@gmail.com>
2024-09-30 14:39:49 +0200

    Fix issue where selecting new request method would not show the BODY tab

--- studio/src/pages/RequestorPage/store/slices/requestResponseSlice.ts
+++ studio/src/pages/RequestorPage/store/slices/requestResponseSlice.ts
@@ -13,6 +13,7 @@ import {
   removeBaseUrl,
 } from "../utils";
 import type { RequestResponseSlice, Store } from "./types";
+import { getVisibleRequestPanelTabs } from "../tabs";
 
 export const requestResponseSlice: StateCreator<
   Store,
@@ -73,6 +74,20 @@ export const requestResponseSlice: StateCreator<
       // Update other state properties based on the new method and request type
       // (e.g., activeRoute, visibleRequestsPanelTabs, activeRequestsPanelTab, etc.)
       // You might want to move some of this logic to separate functions or slices
+      
+
+      // Update visibleRequestsPanelTabs based on the new method and request type
+      state.visibleRequestsPanelTabs = getVisibleRequestPanelTabs({
+        requestType,
+        method,
+      });
+
+      // Ensure the activeRequestsPanelTab is valid
+      state.activeRequestsPanelTab = state.visibleRequestsPanelTabs.includes(
+        state.activeRequestsPanelTab
+      )
+        ? state.activeRequestsPanelTab
+        : state.visibleRequestsPanelTabs[0];
     }),
 
   setPathParams: (pathParams) =>
