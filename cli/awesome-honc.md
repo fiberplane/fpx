b8e2872c28b3ffc1393037c5c7d4371ea10bef01
Brett Beutell <brbeut@gmail.com>
2024-10-15 04:51:13 -0700

    Plop in create-honc-app cli

---
+++ cli/awesome-honc.md
@@ -0,0 +1,7 @@
+# Awesome HONC
+A curated list of awesome [HONC](www.honc.dev) guides, recipes and other resources.
+
+## HONC usecases
+- [AI architecture: vector database per tentant](https://github.com/neondatabase/db-per-tenant)
+
+
---
+++ cli/src/utils.ts
@@ -0,0 +1,42 @@
+import { cancel, log } from "@clack/prompts";
+import { spawn } from "node:child_process";
+import { CANCEL_MESSAGE } from "./const";
+import fs from "node:fs";
+
+export function getPackageManager() {
+  return process.env.npm_config_user_agent?.split("/").at(0);
+}
+
+export async function runShell(cwd: string, commands: string[]): Promise<void> {
+  const commandStr = commands.join(" ");
+
+  return new Promise((resolve, reject) => {
+    const child = spawn(commandStr, [], { cwd, shell: true, timeout: 60000 });
+
+    child.on("error", (error) => {
+      reject(error);
+    });
+
+    child.on("exit", (code) => {
+      if (code === 0) {
+        resolve();
+      } else {
+        reject(new Error(`Command failed with exit code ${code}`));
+      }
+    });
+
+    // Swallow stdout and stderr
+    child.stdout.on("data", () => {});
+    child.stderr.on("data", () => {});
+  });
+}
+
+export function handleError(error: Error) {
+  log.error(`create-honc-app exited with an error: ${error.message}`);
+  process.exit(1);
+}
+
+export function handleCancel() {
+  cancel(CANCEL_MESSAGE);
+  process.exit(0);
+}
