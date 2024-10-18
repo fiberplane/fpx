5c0e1d5a58b61987f9b40f6689c2740bbccd7e04
Brett Beutell <brbeut@gmail.com>
2024-10-18 07:13:27 -0700

    Add project description to package.json

--- cli/src/project-name.ts
+++ cli/src/project-name.ts
@@ -3,6 +3,11 @@ import { join } from "node:path";
 import type { Context } from "./context";
 import { getProjectName } from "./utils";
 
+/**
+ * Update the project name in the package.json and wrangler.toml files.
+ * Update the project description in the package.json file.
+ * @param context - The context object containing the project path.
+ */
 export function updateProjectName(context: Context): void {
   if (!context.path) {
     return;
@@ -21,6 +26,9 @@ export function updateProjectName(context: Context): void {
     try {
       const packageJson = JSON.parse(readFileSync(packageJsonPath, "utf-8"));
       packageJson.name = projectName;
+      if (context.description?.trim()) {
+        packageJson.description = context.description?.trim();
+      }
       writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2));
     } catch {
       // Fail silently
