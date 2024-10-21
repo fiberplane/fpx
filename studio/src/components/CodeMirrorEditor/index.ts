ed877c163ea9f3e8e20880724d0b92cdb86177e4
Laurynas Keturakis <laurynas.keturakis@gmail.com>
2024-10-21 09:51:56 -0700

    add codemirror prompt componenet

--- studio/src/components/CodeMirrorEditor/index.ts
+++ studio/src/components/CodeMirrorEditor/index.ts
@@ -2,3 +2,4 @@ export { CodeMirrorSqlEditor } from "./CodeMirrorSqlEditor";
 export { CodeMirrorTypescriptEditor } from "./CodeMirrorTypescriptEditor";
 export { CodeMirrorJsonEditor } from "./CodeMirrorJsonEditor";
 export { CodeMirrorInput } from "./CodeMirrorInput";
+export { CodeMirrorPrompt } from "./CodeMirrorPrompt";
