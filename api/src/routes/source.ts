961ac55f845e13f45c75d4ae536bebc381640b4c
Brett Beutell <brbeut@gmail.com>
2024-11-18 15:21:51 +0100

    Remove unused api route for finding source function from error

--- api/src/routes/source.ts
+++ api/src/routes/source.ts
@@ -1,52 +1,11 @@
-import { readFileSync } from "node:fs";
-import { zValidator } from "@hono/zod-validator";
 import { Hono } from "hono";
 import { cors } from "hono/cors";
-import { SourceMapConsumer } from "source-map";
-import { z } from "zod";
 
 import { findSourceFunctions } from "../lib/find-source-function/index.js";
 import type { Bindings, Variables } from "../lib/types.js";
 import logger from "../logger/index.js";
 
 const app = new Hono<{ Bindings: Bindings; Variables: Variables }>();
-
-app.get(
-  "/v0/source",
-  cors(),
-  zValidator(
-    "query",
-    z.object({ source: z.string(), line: z.string(), column: z.string() }),
-  ),
-  async (ctx) => {
-    const { source, line, column } = ctx.req.query();
-
-    try {
-      const file = JSON.parse(readFileSync(source, "utf8").toString());
-      const consumer = await new SourceMapConsumer(file);
-      const pos = consumer.originalPositionFor({
-        line: Number.parseInt(line, 10),
-        column: Number.parseInt(column, 10),
-      });
-      consumer.destroy();
-
-      return ctx.json(pos);
-    } catch (err) {
-      const message = getValueFromObject(err, "message", "Unknown error");
-      const name = getValueFromObject(err, "name", "");
-
-      logger.error("Could not read source file", message);
-      return ctx.json(
-        {
-          error: "Error reading file",
-          name,
-          message,
-        },
-        500,
-      );
-    }
-  },
-);
 
 app.post("/v0/source-function", cors(), async (ctx) => {
   const { handler, source } = ctx.req.query();
