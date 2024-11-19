23efcd57c0fb0fcc29bf63a125131db821268663
Brett Beutell <brbeut@gmail.com>
2024-11-19 19:21:33 +0100

    Add simple openapi example app

---
+++ examples/openapi-zod/README.md
@@ -0,0 +1,18 @@
+## Overview
+
+This is an implementation of the Hono-Zod-OpenAPI integration from the Hono docs.
+
+## Commands
+
+```sh
+# HACK - This script initializes a D1 database *locally* so that we can mess with it
+pnpm db:touch
+pnpm db:generate
+pnpm db:migrate
+```
+
+```sh
+pnpm i
+pnpm dev
+```
+
---
+++ examples/openapi-zod/src/index.ts
@@ -0,0 +1,80 @@
+import { instrument } from "@fiberplane/hono-otel";
+import { drizzle } from "drizzle-orm/d1";
+import { OpenAPIHono } from '@hono/zod-openapi'
+import { createRoute, z } from '@hono/zod-openapi'
+import * as schema from "./db/schema";
+
+type Bindings = {
+  DB: D1Database;
+};
+
+const app = new OpenAPIHono<{ Bindings: Bindings }>();
+
+const ParamsSchema = z.object({
+  id: z
+    .string()
+    .min(3)
+    .openapi({
+      param: {
+        name: 'id',
+        in: 'path',
+      },
+      example: '1212121',
+    }),
+})
+
+const UserSchema = z
+  .object({
+    id: z.string().openapi({
+      example: '123',
+    }),
+    name: z.string().openapi({
+      example: 'John Doe',
+    }),
+    age: z.number().openapi({
+      example: 42,
+    }),
+  })
+  .openapi('User')
+
+const route = createRoute({
+  method: 'get',
+  path: '/users/{id}',
+  request: {
+    params: ParamsSchema,
+  },
+  responses: {
+    200: {
+      content: {
+        'application/json': {
+          schema: UserSchema,
+        },
+      },
+      description: 'Retrieve the user',
+    },
+  },
+})
+
+app.openapi(route, (c) => {
+  const { id } = c.req.valid('param')
+  return c.json({
+    id,
+    age: 20,
+    name: 'Ultra-user',
+  })
+})
+
+// The OpenAPI documentation will be available at /doc
+app.doc('/doc', {
+  openapi: '3.0.0',
+  info: {
+    version: '1.0.0',
+    title: 'My API',
+  },
+})
+
+app.get("/", (c) => {
+  return c.text("Hello Hono OpenAPI!");
+});
+
+export default instrument(app);
