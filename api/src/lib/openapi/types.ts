c23c52f0f67be7fa85b520c4e6eddcc3e6179e84
Brett Beutell <brbeut@gmail.com>
2024-11-29 13:46:45 +0100

    Remove dead code

--- api/src/lib/openapi/types.ts
+++ api/src/lib/openapi/types.ts
@@ -30,50 +30,8 @@ export function isOpenApiSpec(value: unknown): value is OpenApiSpec {
   if (!result.success) {
     logger.error(
       "[isOpenApiSpec] Error parsing truthy OpenAPI spec:",
-      // JSON.stringify(result.error.format(), null, 2),
       JSON.stringify(result.error.issues, null, 2),
     );
   }
   return result.success;
-}
-
-export function validateReferences(spec: OpenApiSpec): boolean {
-  const refs = Array.from(
-    spec.components?.schemas ? Object.keys(spec.components.schemas) : [],
-  );
-  let isValid = true;
-
-  for (const pathItem of Object.values(spec.paths)) {
-    for (const operation of Object.values(pathItem)) {
-      for (const param of operation.parameters ?? []) {
-        if (param.schema?.$ref) {
-          const refName = param.schema.$ref.split("/").pop();
-          if (refName && !refs.includes(refName)) {
-            logger.error(
-              `Reference ${param.schema.$ref} not found in components.schemas`,
-            );
-            isValid = false;
-          }
-        }
-      }
-
-      if (operation.requestBody?.content) {
-        for (const content of Object.values(operation.requestBody.content)) {
-          if (content.schema?.$ref) {
-            const refName = content.schema.$ref.split("/").pop();
-            if (refName && !refs.includes(refName)) {
-              logger.error(
-                `Reference ${content.schema.$ref} not found in components.schemas`,
-              );
-              isValid = false;
-            }
-          }
-        }
-      }
-
-      // Similarly validate responses and other $ref usages
-    }
-  }
-
-  return isValid;
 }
