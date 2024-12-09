4bfb9fb20d73b0381ed9c41a0120b7272a4def9e
Brett Beutell <brbeut@gmail.com>
2024-12-09 18:31:06 +0100

    Try removing possibly erroneous code

--- studio/src/pages/RequestorPage/store/slices/requestResponseSlice.ts
+++ studio/src/pages/RequestorPage/store/slices/requestResponseSlice.ts
@@ -207,14 +207,14 @@ export const requestResponseSlice: StateCreator<
   activeResponse: null,
   showResponseBodyFromHistory: (traceId) =>
     set((state) => {
-      state.activeHistoryResponseTraceId = traceId;
-      // Recall that an 'active response' is one for which we will have the body we received from the service
-      // This means it can contain binary data, whereas the history response will not
-      // We should prefer to keep the active response as response to render, so long as its traceId matches the current history response traceId
-      const activeTraceId = state.activeResponse?.traceId;
-      if (!activeTraceId || activeTraceId !== traceId) {
-        state.activeResponse = null;
-      }
+      // state.activeHistoryResponseTraceId = traceId;
+      // // Recall that an 'active response' is one for which we will have the body we received from the service
+      // // This means it can contain binary data, whereas the history response will not
+      // // We should prefer to keep the active response as response to render, so long as its traceId matches the current history response traceId
+      // const activeTraceId = state.activeResponse?.traceId;
+      // if (!activeTraceId || activeTraceId !== traceId) {
+      //   state.activeResponse = null;
+      // }
     }),
   clearResponseBodyFromHistory: () =>
     set((state) => {
