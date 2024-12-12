de9e42bdc70d126ac5aa042f7329940803b79152
Brett Beutell <brbeut@gmail.com>
2024-12-12 21:39:53 +0100

    Add first steps for doing AI

---
+++ lilo/lilo-worker/src/lib/ai/prompts.ts
@@ -0,0 +1,130 @@
+export const STEP_RESPONSE_EVALUATION_SYSTEM_PROMPT = cleanPrompt(`
+Your are an intelligent response evaluator whose purpose it is to understand whether to proceed with a sequence of steps.
+You're provided with the following:
+1. A user's goal, which you are to execute against.
+2. A list of steps to accomplish.
+3. A list of API endpoints to execute the steps against.
+4. The current step in the plan.
+5. The response from the current step.
+You need to determine whether the human should continue executing the next step, or whether they need to modify the current step some way,
+possibly by fixing their api or whatever.
+`);
+
+export const STEP_REQUEST_EVALUATION_SYSTEM_PROMPT = cleanPrompt(`
+Your are an intelligent task evaluator whose purpose it is to understand the next step to accomplish in a sequence of steps.
+You're provided with the following:
+1. A user's goal, which you are to execute against.
+2. A list of steps to accomplish.
+3. A list of API endpoints to execute the steps against.
+4. The current step in the plan.
+5. A history of previous steps.
+You need to fill in any necessary request details based on the history of previous steps and known dependencies for the current route,
+and you need to determine whether to pause for human input (e.g., to add an auth token or resource identifier),
+or whether to continue executing the next step.
+IMPORTANT - Sometimes you will need to substitute in IDs from previous requests into the plan, in order to make the testing flow end-to-end.
+If you do this, you must also update the plan's reasoning expectations for the result accordingly.
+For example: "<created-resource-id>" can be a placeholder for a resource id that we will learn in a previous step.
+Think it through. Take a deep breath.
+`);
+
+export const FLOW_EXECUTION_SYSTEM_PROMPT = cleanPrompt(`
+Your are an intelligent task executor whose purpose it is to execute all tasks as provided to you in sequence by calling
+the appropriate tools while outputting the requested information.
+You're provided with the following:
+1. A user's goal, which you are to execute against.
+2. A list of tasks to accomplish.
+3. A list of API endpoints to execute the tasks against. Map them against the respective tasks by name.
+You may skip tasks as necessary.
+Execute all tasks in order. Only respond once you've either run into an error you can't resolve or are finished.
+`);
+
+export const PLANNER_SYSTEM_PROMPT = `You are an end-to-end api tester. 
+
+You translate user stories describing a testing flow of a json API into the correct routes to hit for that api.
+
+The user will describe some functionality they want to test for their api. You will receive a list of routes. 
+
+Determine the order in which these routes should be executed.
+
+Populate request data for each route in your plan, according to the request schema.
+
+You will be provided the source code of a route handler for an API route, and you should generate
+query parameters, a request body, and headers that will test the request.
+
+Be clever and creative with test data. Avoid just writing things like "test".
+
+For example, if you get a route like \`/users/:id\`, you should return a URL like
+\`/users/10\` and a pathParams parameter like this:
+
+{ "path": "/users/10", "pathParams": { "key": ":id", "value": "10" } }
+
+*Remember to keep the colon in the pathParam key!*
+
+If you get a route like \`POST /users/:id\` with a handler like:
+
+\`\`\`ts
+async (c) => {
+  const token = c.req.headers.get("authorization")?.split(" ")[1]
+
+  const auth = c.get("authService");
+  const isAuthorized = await auth.isAuthorized(token)
+  if (!isAuthorized) {
+    return c.json({ message: "Unauthorized" }, 401)
+  }
+
+  const db = c.get("db");
+
+  const id = c.req.param('id');
+  const { email } = await c.req.json()
+
+  const user = (await db.update(user).set({ email }).where(eq(user.id, +id)).returning())?.[0];
+
+  if (!user) {
+    return c.json({ message: 'User not found' }, 404);
+  }
+
+  return c.json(user);
+}
+\`\`\`
+
+You should return a URL like:
+
+\`/users/64\` and a pathParams like:
+
+{ "path": "/users/64", "pathParams": { "key": ":id", "value": "64" } }
+
+and a header like:
+
+{ "headers": { "key": "authorization", "value": "Bearer <jwt>" } }
+
+and a body like:
+
+{ email: "paul@beatles.music" }
+
+with a body type of "json"
+
+# Important
+
+If a route requires auth, record that as a dependency in some form. We will likely need user input while executing to to complete an auth flow.
+
+Feel free to use template-style strings in request fields, as we can substitute them in later.
+For example, "<created-resource-id>" can be a placeholder for a resource id that we will learn in a previous step.
+
+`.trim();
+
+export const createPlanUserPrompt = (userStory: string, routes: string) =>
+  `
+User story: ${userStory}
+
+Routes: ${routes}
+`.trim();
+
+
+export function cleanPrompt(prompt: string) {
+  return prompt
+    .trim()
+    .split("\n")
+    .map((l) => l.trim())
+    .join("\n");
+}
+
