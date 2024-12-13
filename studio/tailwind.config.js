beb341592c8faa0846bf0b166b11d5aeb220044d
Jacco Flenter <flenter@users.noreply.github.com>
2024-12-13 11:53:36 +0100

    Use import instead of require in tailwind.config

--- studio/tailwind.config.js
+++ studio/tailwind.config.js
@@ -1,3 +1,5 @@
+import tailwindcssAnimate from "tailwindcss-animate";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   darkMode: ["selector"],
@@ -93,5 +95,5 @@ export default {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [tailwindcssAnimate],
 };
