e4fc1d6596c15e46e259eeee3f12c3111deaf885
Brett Beutell <brbeut@gmail.com>
2024-12-19 18:28:50 +0100

    Add tooltip for collection button

--- studio/src/pages/RequestorPage/AddRouteToCollection/AddRouteToCollection.tsx
+++ studio/src/pages/RequestorPage/AddRouteToCollection/AddRouteToCollection.tsx
@@ -4,6 +4,11 @@ import {
   PopoverContent,
   PopoverTrigger,
 } from "@/components/ui/popover";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 import { useHandler } from "@fiberplane/hooks";
 import { Icon } from "@iconify/react";
 import { useState } from "react";
@@ -25,9 +30,16 @@ export function AddRouteToCollection() {
     <Popover open={open} onOpenChange={setOpen}>
       <PopoverTrigger asChild>
         <div>
-          <Button size="icon-xs" variant="ghost">
-            <Icon icon="lucide:folder" />
-          </Button>
+          <Tooltip>
+            <TooltipTrigger>
+              <Button size="icon-xs" variant="ghost">
+                <Icon icon="lucide:folder" />
+              </Button>
+            </TooltipTrigger>
+            <TooltipContent side="left">
+              <p>Add to Collection</p>
+            </TooltipContent>
+          </Tooltip>
         </div>
       </PopoverTrigger>
       <PopoverContent className="w-72 max-w-dvw">
