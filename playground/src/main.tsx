da476aabc2db25a6deccd4362d134ab442b06d68
Laurynas Keturakis <laurynas.keturakis@gmail.com>
2025-01-13 18:08:52 +0100

    add mount path correction

--- playground/src/main.tsx
+++ playground/src/main.tsx
@@ -5,7 +5,16 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { routeTree } from "./routeTree.gen";
 import "./index.css";
 
-const router = createRouter({ routeTree });
+const rootElement = document.getElementById("root");
+if (rootElement === null) {
+  throw new Error("Root element not found");
+}
+
+// NOTE: Mount path defines which path the whole playground is mounted on. The
+// client router needs to know this so it can generate correct links
+const mountPath = rootElement.getAttribute("data-mount-path");
+
+const router = createRouter({ routeTree, basepath: mountPath ?? undefined });
 const queryClient = new QueryClient();
 
 // Register your router for maximum type safety
@@ -13,11 +22,6 @@ declare module "@tanstack/react-router" {
   interface Register {
     router: typeof router;
   }
-}
-
-const rootElement = document.getElementById("root");
-if (rootElement === null) {
-  throw new Error("Root element not found");
 }
 
 createRoot(rootElement).render(
