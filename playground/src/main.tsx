ae6327c62301a72977c66eb3ea38495ad3ccc642
Brett Beutell <brbeut@gmail.com>
2025-01-13 16:42:37 +0100

    Format

--- playground/src/main.tsx
+++ playground/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 
+// biome-ignore lint/style/noNonNullAssertion: dgaf
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
