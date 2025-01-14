83ce49b8d4f6200dc92538d271ba0fa4ff6125ad
Brett Beutell <brbeut@gmail.com>
2025-01-14 13:37:27 +0100

    Default to system theme

--- playground/src/components/playground/Playground.tsx
+++ playground/src/components/playground/Playground.tsx
@@ -9,7 +9,7 @@ export function Playground() {
   const navigationItems = getNavigationData();
 
   return (
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+    <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
       <div className="min-h-screen bg-background">
         <div className="flex">
           {/* Sidebar */}
