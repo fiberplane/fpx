a969114fb269b82ca70712fbaad96a5077a1c9f9
Brett Beutell <brbeut@gmail.com>
2025-01-15 12:17:11 +0100

    Update root biome config for playground stuff

--- playground/src/components/mode-toggle.tsx
+++ playground/src/components/mode-toggle.tsx
@@ -1,16 +1,16 @@
-import { Moon, Sun } from "lucide-react"
+import { Moon, Sun } from "lucide-react";
 
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
-import { useTheme } from "./theme-provider"
+} from "@/components/ui/dropdown-menu";
+import { useTheme } from "./theme-provider";
 
 export function ModeToggle() {
-  const { setTheme } = useTheme()
+  const { setTheme } = useTheme();
 
   return (
     <DropdownMenu>
@@ -33,5 +33,5 @@ export function ModeToggle() {
         </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
-  )
-} 
+  );
+}
