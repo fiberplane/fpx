86aa660fbe0b331c34375d2fddb7ab156267fe63
Brett Beutell <brbeut@gmail.com>
2025-01-16 15:34:21 +0100

    Remove nodejs support

--- packages/embedded/src/router.tsx
+++ packages/embedded/src/router.tsx
@@ -1,20 +1,7 @@
-/** @jsx jsx */
-/** @jsxImportSource hono/jsx */
-// @ts-nocheck
-import { jsx } from "hono/jsx";
-
-import { existsSync, readFileSync } from "node:fs";
-import path, { dirname, join } from "node:path";
-import { fileURLToPath } from "node:url";
 import { type Env, Hono } from "hono";
 import { html, raw } from "hono/html";
 import type { OpenAPIV3, OpenAPIV3_1 } from "openapi-types";
 import type { EmbeddedMiddlewareOptions } from "./index.js";
-
-// TODO: This only works with node, fix asset loading for other runtimes as well
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-const clientDistPath = join(__dirname, "../../../playground/dist");
 
 interface RouterOptions extends EmbeddedMiddlewareOptions {
   mountedPath: string;
@@ -27,44 +14,9 @@ export function createRouter<E extends Env>({
 }: RouterOptions): Hono<E> {
   const router = new Hono<E>();
 
-  // TODO: This only works with node, fix asset loading for other runtimes as well
-  // Skip the file handler when CDN is set, this one should be dropped eventually anyways
-  if (!cdn) {
-    router.get("/client/:file", async (c) => {
-      const file = c.req.param("file");
-      const filePath = join(clientDistPath, file);
+  const cssBundleUrl = new URL("index.css", cdn).href;
 
-      if (!existsSync(filePath)) {
-        return c.text("File not found", 404);
-      }
-
-      try {
-        const content = readFileSync(filePath, "utf-8");
-        const contentType = file.endsWith(".js")
-          ? "application/javascript"
-          : "text/css";
-
-        return new Response(content, {
-          status: 200,
-          headers: {
-            "Content-Type": contentType,
-            "Cache-Control": "no-store, no-cache, must-revalidate",
-            Pragma: "no-cache",
-          },
-        });
-      } catch (error) {
-        return c.text("Error reading file", 500);
-      }
-    });
-  }
-
-  const cssBundleUrl = cdn
-    ? new URL("index.css", cdn).href
-    : path.resolve(mountedPath, "client/index.css");
-
-  const jsBundleUrl = cdn
-    ? new URL("index.js", cdn).href
-    : path.resolve(mountedPath, "client/index.js");
+  const jsBundleUrl = new URL("index.js", cdn).href;
 
   router.get("/*", async (c) => {
     const resolvedSpec = await resolveSpec(spec);
