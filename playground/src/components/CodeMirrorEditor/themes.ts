c6d7b0ab4551a4328148ef603821c63f0367689b
Brett Beutell <brbeut@gmail.com>
2025-01-17 11:47:39 +0100

    Update json editor styles

--- playground/src/components/CodeMirrorEditor/themes.ts
+++ playground/src/components/CodeMirrorEditor/themes.ts
@@ -6,6 +6,12 @@ export const customTheme = EditorView.theme({
     // HACK
     background: "transparent !important",
   },
+  ".ͼq": {
+    color: "hsl(var(--info))",
+  },
+  ".ͼu": {
+    color: "hsl(var(--success))",
+  },
 });
 
 export const duotonePlaintextOverride = EditorView.theme({
