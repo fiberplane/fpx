aa929dffa05a595749f17f5d1a8703f89ff1fef1
Laurynas Keturakis <laurynas.keturakis@gmail.com>
2025-01-23 20:33:29 +0100

    rework the interface for embedded playground mounting - serialize all options into data-options attribute on <div root>

--- packages/embedded/src/middleware.ts
+++ packages/embedded/src/middleware.ts
@@ -1,34 +1,24 @@
 import type { Context } from "hono";
 import type { Env, MiddlewareHandler } from "hono/types";
-import type { OpenAPIV3, OpenAPIV3_1 } from "openapi-types";
-import { type RouterSpec, createRouter } from "./router.js";
+import { createRouter } from "./router.js";
+import type { EmbeddedOptions, ResolvedEmbeddedOptions } from "./types.js";
 
 // HACK - We need to manually update the version in the CDN URL when you release a new version
 //        Eventually we should do this automagically when building the package (and derive the version from the package.json)
 const VERSION = "0.0.19";
 const CDN_URL = `https://cdn.jsdelivr.net/npm/@fiberplane/embedded@${VERSION}/dist/playground/`;
 
-export interface EmbeddedMiddlewareOptions {
-  apiKey: string;
-  cdn?: string;
-  spec?: OpenAPIV3_1.Document | OpenAPIV3.Document | string;
-}
-
 export const createMiddleware =
-  <E extends Env>(options: EmbeddedMiddlewareOptions): MiddlewareHandler<E> =>
+  <E extends Env>(options: EmbeddedOptions): MiddlewareHandler<E> =>
   async (c, next) => {
     const { mountedPath, internalPath } = getPaths(c);
 
-    const origin = new URL(c.req.url).origin;
-    const modSpec: RouterSpec = parseSpecParameter(options?.spec, origin);
-
     // Forward request to embedded router, continuing middleware chain if no route matches
     const router = createRouter({
-      ...options,
+      cdn: options.cdn ?? CDN_URL,
       mountedPath,
-      cdn: options?.cdn ?? CDN_URL,
-      spec: modSpec,
-    });
+      ...options,
+    } satisfies ResolvedEmbeddedOptions);
 
     // Create a new request with the corrected (internal) path
     const newUrl = new URL(c.req.url);
@@ -54,39 +44,5 @@ function getPaths(c: Context): { mountedPath: string; internalPath: string } {
   return {
     mountedPath,
     internalPath,
-  };
-}
-
-function parseSpecParameter(
-  spec: OpenAPIV3_1.Document | OpenAPIV3.Document | string | undefined,
-  origin: string,
-): RouterSpec {
-  if (!spec) {
-    return {
-      type: "empty",
-      value: undefined,
-      origin,
-    };
-  }
-  if (typeof spec === "string" && spec.startsWith("/")) {
-    return {
-      type: "path",
-      value: spec,
-      origin,
-    };
-  }
-  if (spec && typeof spec === "object") {
-    return {
-      type: "raw",
-      value: spec,
-      origin,
-    };
-  }
-  // NOTE - We assume the spec is a URL if it's not a path-like string or an object
-  // TODO - Make this more robust, add an unknown type
-  return {
-    type: "url",
-    value: spec,
-    origin,
   };
 }
