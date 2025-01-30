702790fa01e7ce6811871379ba4ece175e42689a
Brett Beutell <brbeut@gmail.com>
2025-01-30 10:54:42 +0100

    Update playgroudn vite config to be able to configure the proxy url for the embedded api

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -4,6 +4,13 @@ import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 import svgr from "vite-plugin-svgr";
 
+// The SPA, when running locally, needs to proxy requests to the embedded API sometimes
+// It's nice to be able to configure this.
+// E.g., if you're running a sample API on localhost:6242 instead of localhost:8787, you can set EMBEDDED_API_URL=http://localhost:6242/fp
+// to make the SPA proxy requests to your local API with the @fiberplane/embedded package.
+const EMBEDDED_API_URL =
+  process.env.EMBEDDED_API_URL ?? "http://localhost:8787/fp";
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
@@ -22,7 +29,7 @@ export default defineConfig({
   server: {
     port: 6660,
     proxy: {
-      "/api": "http://localhost:8787/fp",
+      "/api": EMBEDDED_API_URL,
     },
   },
   // NOTE: Consistent filenames (without hashes) make sure we can load the assets via cdn (from @fiberplane/embedded)
