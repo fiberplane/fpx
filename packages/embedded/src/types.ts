fd764623f2e2172b3a8c05cb9c5c6e10c01c82cd
Brett Beutell <brbeut@gmail.com>
2025-02-10 18:16:39 +0100

    Modify logIfDebug to accept Context param as well

--- packages/embedded/src/types.ts
+++ packages/embedded/src/types.ts
@@ -1,5 +1,4 @@
-import { Hono } from "hono";
-import type { Env } from "hono/types";
+import type { Context } from "hono";
 
 export interface EmbeddedOptions {
   /**
@@ -51,12 +50,22 @@ export interface FiberplaneAppType {
   };
 }
 
+export function logIfDebug(debug: boolean, message: unknown): void;
+export function logIfDebug(debug: Context, message: unknown): void;
 export function logIfDebug(
-  debug: boolean,
-  message?: unknown,
+  debug: boolean | Context,
+  message: unknown,
   ...params: unknown[]
-) {
-  if (debug) {
-    console.debug(message, ...params);
+): void {
+  try {
+    // If debug is a boolean, we use it directly
+    // If "debug" is a Context, we check the debug flag from the context's variable's map
+    const debugEnabled =
+      typeof debug === "boolean" ? debug : !!debug?.get("debug");
+    if (debugEnabled) {
+      console.debug("[fiberplane:debug] ", message, ...params);
+    }
+  } catch {
+    // If the debug log failed for whatever reason, we'll just swallow the error
   }
 }
