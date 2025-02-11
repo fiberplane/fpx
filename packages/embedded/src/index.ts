d8bdef4739e818dc498a6e5b0beec99e9cbb3d56
Brett Beutell <brbeut@gmail.com>
2025-02-11 08:49:53 +0100

    Still export createMiddleware so we do not break the readmes we gave to people

--- packages/embedded/src/index.ts
+++ packages/embedded/src/index.ts
@@ -1,2 +1,11 @@
-export { createFiberplane } from "./middleware.js";
+import { createFiberplane } from "./middleware.js";
+
+// Export the main function
+export { createFiberplane };
+
+/**
+ * @deprecated Use createFiberplane() instead. This alias will be removed in a future version.
+ */
+export const createMiddleware = createFiberplane;
+
 export { FpService } from "./services/index.js";
