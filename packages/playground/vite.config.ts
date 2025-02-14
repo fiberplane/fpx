5d1664a0f3cfe986d0ca246a948c078b93d86b3a
Laurynas Keturakis <laurynas.keturakis@gmail.com>
2025-02-14 16:41:30 +0100

    use dotenv for vite.config

--- packages/playground/vite.config.ts
+++ packages/playground/vite.config.ts
@@ -5,13 +5,16 @@ import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 import { nodePolyfills } from "vite-plugin-node-polyfills";
 import svgr from "vite-plugin-svgr";
+import { config } from "dotenv";
+
+config({path: "./.dev.vars"});
 
 // The SPA, when running locally, needs to proxy requests to the embedded API sometimes
 // It's nice to be able to configure this.
 // E.g., if you're running a sample API on localhost:6242 instead of localhost:8787, you can set EMBEDDED_API_URL=http://localhost:6242/fp
 // to make the SPA proxy requests to your local API with the @fiberplane/hono package.
 const EMBEDDED_API_URL =
-  process.env.EMBEDDED_API_URL ?? "http://localhost:8787/fp";
+  process.env.EMBEDDED_API_URL ?? "http://localhost:7676/api/fp";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -25,6 +28,9 @@ export default defineConfig({
       include: "**/*.svg",
     }),
   ],
+  optimizeDeps: {
+    exclude: ["@iconify/react"],
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
@@ -33,8 +39,14 @@ export default defineConfig({
   server: {
     port: 6660,
     proxy: {
-      "/api": EMBEDDED_API_URL,
+      "/api": {
+        target: EMBEDDED_API_URL,
+        headers: {
+          Authorization: `Bearer ${process.env.FIBERPLANE_API_KEY}`,
+        }
+      },
     },
+    cors: true,
   },
   // NOTE: Consistent filenames (without hashes) make sure we can load the assets via cdn (from @fiberplane/hono)
   build: {
